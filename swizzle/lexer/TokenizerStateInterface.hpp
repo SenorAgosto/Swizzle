427af1499a079b713e2b391e5a42c6366c033bc5
Austin Gilbert <ceretullis@gmail.com>
2017-05-06 23:16:58 -0500

    lexer: initial definition of TokenizerStateInterface

---
+++ swizzle/lexer/TokenizerStateInterface.hpp
@@ -0,0 +1,19 @@
+#pragma once 
+#include <boost/utility/string_view.hpp>
+#include <cstddef>
+
+namespace swizzle { namespace lexer {
+    class FileInfo;
+    class Token;
+    enum class TokenizerState : std::uint8_t;
+}}
+
+namespace swizzle { namespace lexer {
+
+    class TokenizerStateInterface
+    {
+    public:
+        virtual ~TokenizerStateInterface(){}
+        virtual TokenizerState consume(const boost::string_view& source, const std::size_t position, FileInfo& filePosition, Token& token) const = 0;
+    };
+}}

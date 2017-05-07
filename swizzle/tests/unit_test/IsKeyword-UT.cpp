086efd4d1052ce0ad4c3d06dd5d7880759703dc6
Austin Gilbert <ceretullis@gmail.com>
2017-05-07 18:17:43 -0500

    Adding a helper to identify keyword tokens.

---
+++ swizzle/tests/unit_test/IsKeyword-UT.cpp
@@ -0,0 +1,30 @@
+#include "./platform/UnitTestSupport.hpp"
+#include <swizzle/IsKeyword.hpp>
+
+namespace {
+
+    using namespace swizzle;
+
+    TEST(verifyIsKeyword)
+    {
+        CHECK(IsKeyword("bitfield"));
+        CHECK(IsKeyword("const"));
+        CHECK(IsKeyword("enum"));
+        CHECK(IsKeyword("import"));
+        CHECK(IsKeyword("namespace"));
+
+        CHECK(IsKeyword("struct"));
+        CHECK(IsKeyword("using"));
+
+        CHECK(IsKeyword("u8"));
+        CHECK(IsKeyword("i8"));
+        CHECK(IsKeyword("u16"));
+        CHECK(IsKeyword("i16"));
+        CHECK(IsKeyword("u32"));
+        CHECK(IsKeyword("i32"));
+        CHECK(IsKeyword("u64"));
+        CHECK(IsKeyword("i64"));
+
+        CHECK(IsKeyword("variable_block"));
+    }
+}

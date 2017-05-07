c24713f9dc22d58936619df5b5e6d4bde3d356e0
Austin Gilbert <ceretullis@gmail.com>
2017-05-06 23:15:38 -0500

    lexer: adding an ostream<< operator for TokenizerState for unit testing

---
+++ swizzle/lexer/TokenizerState.hpp
@@ -0,0 +1,22 @@
+#pragma once 
+#include <cstdint>
+#include <ostream>
+
+namespace swizzle { namespace lexer {
+
+    enum class TokenizerState : std::uint8_t {
+        Init,
+        FirstSlash,
+        Comment,
+        MultilineComment,
+        StringLiteral,
+        CharLiteral,
+        NumericLiteral,
+        EscapedCharInCharLiteral,
+        EndCharLiteral,
+        EscapedCharInStringLiteral,
+        SyntaxError,
+    };
+
+    std::ostream& operator<<(std::ostream& os, const TokenizerState state);
+}}
---
+++ swizzle/src/lexer/TokenizerState.cpp
@@ -0,0 +1,23 @@
+#include <swizzle/lexer/TokenizerState.hpp>
+
+namespace swizzle { namespace lexer {
+
+    std::ostream& operator<<(std::ostream& os, const TokenizerState state)
+    {
+        switch(state)
+        {
+        case TokenizerState::Init:                          return os << "TokenizerState::Init";
+        case TokenizerState::FirstSlash:                    return os << "TokenizerState::FirstSlash";
+        case TokenizerState::Comment:                       return os << "TokenizerState::Comment";
+        case TokenizerState::MultilineComment:              return os << "TokenizerState::MultilineComment";
+        case TokenizerState::StringLiteral:                 return os << "TokenizerState::StringLiteral";
+        case TokenizerState::CharLiteral:                   return os << "TokenizerState::CharLiteral";
+        case TokenizerState::NumericLiteral:                return os << "TokenizerState::NumericLiteral";
+        case TokenizerState::EscapedCharInCharLiteral:      return os << "TokenizerState::EscapedCharInCharLiteral";
+        case TokenizerState::EndCharLiteral:                return os << "TokenizerState::EndCharLiteral";
+        case TokenizerState::EscapedCharInStringLiteral:    return os << "TokenizerState::EscapedCharInStringLiteral";
+        case TokenizerState::SyntaxError:                   return os << "TokenizerState::SyntaxError";
+        default:                                            return os << "Unknown TokenizerState value";
+        };
+    }
+}}

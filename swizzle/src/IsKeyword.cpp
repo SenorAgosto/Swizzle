57fd248d1dd85945bb129087b0d10cf900f9f266
Austin Gilbert <ceretullis@gmail.com>
2017-05-14 23:10:25 -0500

    lexer: removing built-in types from IsKeyword() helper.

--- swizzle/src/IsKeyword.cpp
+++ swizzle/src/IsKeyword.cpp
@@ -3,7 +3,7 @@
 
 namespace swizzle {
 
-    static const std::regex keywords("import|namespace|using|struct|enum|variable_block|u8|i8|u16|i16|u32|i32|u64|i64|bitfield|const", std::regex::optimize);
+    static const std::regex keywords("import|namespace|using|struct|enum|const", std::regex::optimize);
 
     bool IsKeyword(const boost::string_view& token)
     {

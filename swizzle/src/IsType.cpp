37a2c00c032d8177de3603d732988b48b5bb3e57
Austin Gilbert <ceretullis@gmail.com>
2017-05-14 23:13:27 -0500

    Adding IsType() helper to identify tokens representing built-in types.

---
+++ swizzle/src/IsType.cpp
@@ -0,0 +1,12 @@
+#include <swizzle/IsType.hpp>
+#include <regex>
+
+namespace swizzle {
+
+    static const std::regex keywords("u8|i8|u16|i16|u32|i32|u64|i64|bitfield|variable_block", std::regex::optimize);
+
+    bool IsType(const boost::string_view& token)
+    {
+        return std::regex_match(token.begin(), token.end(), keywords);
+    }
+}
---
+++ swizzle/tests/unit_test/IsType-UT.cpp
@@ -0,0 +1,29 @@
+#include "./platform/UnitTestSupport.hpp"
+#include <swizzle/IsType.hpp>
+
+namespace {
+
+    using namespace swizzle;
+
+    TEST(verifyIsType)
+    {
+        CHECK(!IsType("const"));
+        CHECK(!IsType("enum"));
+        CHECK(!IsType("import"));
+        CHECK(!IsType("namespace"));
+
+        CHECK(!IsType("struct"));
+        CHECK(!IsType("using"));
+
+        CHECK(IsType("bitfield"));
+        CHECK(IsType("u8"));
+        CHECK(IsType("i8"));
+        CHECK(IsType("u16"));
+        CHECK(IsType("i16"));
+        CHECK(IsType("u32"));
+        CHECK(IsType("i32"));
+        CHECK(IsType("u64"));
+        CHECK(IsType("i64"));
+        CHECK(IsType("variable_block"));
+    }
+}

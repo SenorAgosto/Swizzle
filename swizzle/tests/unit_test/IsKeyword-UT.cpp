999fde67787111265fc877bcd6d8e3287b40a42a
Austin Gilbert <ceretullis@gmail.com>
2017-05-14 23:10:25 -0500

    lexer: removing built-in types from IsKeyword() helper.

--- swizzle/tests/unit_test/IsKeyword-UT.cpp
+++ swizzle/tests/unit_test/IsKeyword-UT.cpp
@@ -7,7 +7,6 @@ namespace {
 
     TEST(verifyIsKeyword)
     {
-        CHECK(IsKeyword("bitfield"));
         CHECK(IsKeyword("const"));
         CHECK(IsKeyword("enum"));
         CHECK(IsKeyword("import"));
@@ -16,15 +15,16 @@ namespace {
         CHECK(IsKeyword("struct"));
         CHECK(IsKeyword("using"));
 
-        CHECK(IsKeyword("u8"));
-        CHECK(IsKeyword("i8"));
-        CHECK(IsKeyword("u16"));
-        CHECK(IsKeyword("i16"));
-        CHECK(IsKeyword("u32"));
-        CHECK(IsKeyword("i32"));
-        CHECK(IsKeyword("u64"));
-        CHECK(IsKeyword("i64"));
+        CHECK(!IsKeyword("bitfield"));
+        CHECK(!IsKeyword("u8"));
+        CHECK(!IsKeyword("i8"));
+        CHECK(!IsKeyword("u16"));
+        CHECK(!IsKeyword("i16"));
+        CHECK(!IsKeyword("u32"));
+        CHECK(!IsKeyword("i32"));
+        CHECK(!IsKeyword("u64"));
+        CHECK(!IsKeyword("i64"));
 
-        CHECK(IsKeyword("variable_block"));
+        CHECK(!IsKeyword("variable_block"));
     }
 }

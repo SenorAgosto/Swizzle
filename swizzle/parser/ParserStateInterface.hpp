0dc4a5fdb5c56d44cbb6962138ca49971e5c2832
Austin Gilbert <ceretullis@gmail.com>
2017-05-26 14:57:30 -0500

    parser: removing AST forward declaration from ParserStateInterface

--- swizzle/parser/ParserStateInterface.hpp
+++ swizzle/parser/ParserStateInterface.hpp
@@ -7,16 +7,12 @@ namespace swizzle { namespace lexer {
     class TokenInfo;
 }}
 
-namespace swizzle { namespace ast {
-    class AbstractSyntaxTree;
-}}
-
 namespace swizzle { namespace parser {
 
     class ParserStateInterface
     {
     public:
         virtual ~ParserStateInterface(){}
-        virtual ParserState consume(const lexer::TokenInfo& token, ast::AbstractSyntaxTree& ast, NodeStack& nodeStack, TokenStack& tokenStack) = 0;
+        virtual ParserState consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack) = 0;
     };
 }}

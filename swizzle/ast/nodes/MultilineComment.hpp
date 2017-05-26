cc70eeb095ef13f97cc6f5185987ac23a86ed80f
Austin Gilbert <ceretullis@gmail.com>
2017-05-26 16:08:20 -0500

    ast: adding MultilineComment node

---
+++ swizzle/ast/nodes/MultilineComment.hpp
@@ -0,0 +1,17 @@
+#pragma once 
+#include <swizzle/ast/Node.hpp>
+#include <swizzle/lexer/TokenInfo.hpp>
+
+namespace swizzle { namespace ast { namespace nodes {
+
+    class MultilineComment : public Node
+    {
+    public:
+        MultilineComment(const lexer::TokenInfo& info);
+
+        const lexer::TokenInfo& info() const;
+
+    private:
+        lexer::TokenInfo info_;
+    };
+}}}

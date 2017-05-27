4259dbf3f232352f8315dd2f84fbdadfae96711d
Austin Gilbert <ceretullis@gmail.com>
2017-05-26 23:47:37 -0500

    parser: implementing NamespaceFirstColonState

--- swizzle/src/parser/states/NamespaceFirstColonState.cpp
+++ swizzle/src/parser/states/NamespaceFirstColonState.cpp
@@ -1,14 +1,21 @@
 #include <swizzle/parser/states/NamespaceFirstColonState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState NamespaceFirstColonState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack)
+    ParserState NamespaceFirstColonState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack&)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::colon)
+        {
+            return ParserState::StartNamespace;
+        }
+
+        throw SyntaxError("Expected : found '" + token.token().to_string() + "'", token);
     }
 }}}
---
+++ swizzle/tests/unit_test/Parser-NamespaceFirstColonState-UT.cpp
@@ -0,0 +1,69 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <swizzle/ast/AbstractTreeEdgeVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVertexVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVisitorInterface.hpp>
+
+#include <swizzle/ast/AbstractSyntaxTree.hpp>
+#include <swizzle/parser/states/NamespaceFirstColonState.hpp>
+
+#include <swizzle/Exceptions.hpp>
+
+namespace {
+
+    using namespace swizzle::ast;
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct NamespaceFirstColonStateFixture
+    {
+        NamespaceFirstColonStateFixture()
+        {
+            nodeStack.push(ast.root());
+        }
+
+        states::NamespaceFirstColonState state;
+
+        AbstractSyntaxTree ast;
+        NodeStack nodeStack;
+        TokenStack tokenStack;
+    };
+
+    TEST_FIXTURE(NamespaceFirstColonStateFixture, verifyConstruction)
+    {
+    }
+
+    struct WhenNextTokenIsColon : public NamespaceFirstColonStateFixture
+    {
+        const Token token = Token(":", 0, 1, TokenType::colon);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsColon, verifyConsume)
+    {
+        CHECK_EQUAL(1U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack);
+
+        CHECK_EQUAL(ParserState::StartNamespace, parserState);
+
+        REQUIRE CHECK_EQUAL(1U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsInvalid : public NamespaceFirstColonStateFixture
+    {
+        const Token token = Token(";", 0, 1, TokenType::end_statement);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsInvalid, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack), swizzle::SyntaxError);
+    }
+}

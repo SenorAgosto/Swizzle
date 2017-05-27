fe4fde247484cf854008dec6d3cad4aba19f5075
Austin Gilbert <ceretullis@gmail.com>
2017-05-26 19:55:21 -0500

    parser: implementing StartImportState

---
+++ swizzle/tests/unit_test/Parser-StartImport-UT.cpp
@@ -0,0 +1,74 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <swizzle/ast/AbstractTreeEdgeVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVertexVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVisitorInterface.hpp>
+
+#include <swizzle/ast/AbstractSyntaxTree.hpp>
+#include <swizzle/parser/states/StartImportState.hpp>
+
+#include <swizzle/Exceptions.hpp>
+
+namespace {
+
+    using namespace swizzle::ast;
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct StartImportStateFixture
+    {
+        StartImportStateFixture()
+        {
+            nodeStack.push(ast.root());
+        }
+
+        states::StartImportState state;
+
+        AbstractSyntaxTree ast;
+        NodeStack nodeStack;
+        TokenStack tokenStack;
+    };
+
+    TEST_FIXTURE(StartImportStateFixture, verifyConstruction)
+    {
+    }
+
+    struct WhenNextTokenIsString : public StartImportStateFixture
+    {
+        const Token token = Token("MyType", 0, 6, TokenType::string);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsString, verifyConsume)
+    {
+        CHECK_EQUAL(1U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack);
+
+        CHECK_EQUAL(ParserState::ImportValue, parserState);
+
+        REQUIRE CHECK_EQUAL(1U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(1U, tokenStack.size());
+
+        const auto top = tokenStack.top();
+
+        CHECK_EQUAL(TokenType::string, top.token().type());
+        CHECK_EQUAL("MyType", top.token().to_string());
+    }
+
+    struct WhenNextTokenIsInvalid : public StartImportStateFixture
+    {
+        const Token token = Token(":", 0, 1, TokenType::colon);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsInvalid, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack), swizzle::SyntaxError);
+    }
+}

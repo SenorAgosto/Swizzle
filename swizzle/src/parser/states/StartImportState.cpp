8b15746166a5305e1a000306d63ac0acd532506a
Austin Gilbert <ceretullis@gmail.com>
2017-05-26 19:55:21 -0500

    parser: implementing StartImportState

--- swizzle/src/parser/states/StartImportState.cpp
+++ swizzle/src/parser/states/StartImportState.cpp
@@ -1,14 +1,21 @@
 #include <swizzle/parser/states/StartImportState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState StartImportState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack)
+    ParserState StartImportState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack& tokenStack)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+        if(type == lexer::TokenType::string)
+        {
+            tokenStack.push(token);
+            return ParserState::ImportValue;
+        }
+
+        throw SyntaxError("Expected namespace or type name to import, found: '" + token.token().to_string() + "'", token);
     }
 }}}

ee7494a423a9092e59b087e23a14a6514b1cd469
Austin Gilbert <ceretullis@gmail.com>
2017-05-26 23:39:40 -0500

    parser: implementing StartNamespaceState

--- swizzle/src/parser/states/StartNamespaceState.cpp
+++ swizzle/src/parser/states/StartNamespaceState.cpp
@@ -1,14 +1,22 @@
 #include <swizzle/parser/states/StartNamespaceState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState StartNamespaceState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack)
+    ParserState StartNamespaceState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack& tokenStack)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::string)
+        {
+            tokenStack.push(token);
+            return ParserState::NamespaceValue;
+        }
+
+        throw SyntaxError("Expecting namespace name, found '" + token.token().to_string() + "'", token);
     }
 }}}

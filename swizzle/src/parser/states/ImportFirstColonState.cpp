a10525dd56da1143539919953173f1ad3d4e9ccf
Austin Gilbert <ceretullis@gmail.com>
2017-05-26 20:20:11 -0500

    parser: implementing ImportFirstColonState

--- swizzle/src/parser/states/ImportFirstColonState.cpp
+++ swizzle/src/parser/states/ImportFirstColonState.cpp
@@ -1,14 +1,21 @@
 #include <swizzle/parser/states/ImportFirstColonState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState ImportFirstColonState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack)
+    ParserState ImportFirstColonState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack&)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::colon)
+        {
+            return ParserState::StartImport;
+        }
+
+        throw SyntaxError("Expectd : as a part of a namespace, found '" + token.token().to_string() + "'", token);
     }
 }}}

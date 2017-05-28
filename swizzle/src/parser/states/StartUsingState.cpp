4b8df096453773935057650df8030e9c1fb268e4
Austin Gilbert <ceretullis@gmail.com>
2017-05-28 18:10:30 -0500

    parser: states: implementing StartUsingState

--- swizzle/src/parser/states/StartUsingState.cpp
+++ swizzle/src/parser/states/StartUsingState.cpp
@@ -1,14 +1,44 @@
 #include <swizzle/parser/states/StartUsingState.hpp>
 
+#include <swizzle/Exceptions.hpp>
+#include <swizzle/ast/nodes/TypeAlias.hpp>
+
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/TokenStack.hpp>
+
+#include <swizzle/ast/AbstractTreeEdgeVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVertexVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVisitorInterface.hpp>
+
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <sstream>
 
 namespace swizzle { namespace parser { namespace states {
 
     ParserState StartUsingState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::string)
+        {
+            if(tokenStack.empty())
+            {
+                throw ParserError("Token Stack unexpectedly empty.");
+            }
+
+            const auto& info = tokenStack.top();
+            const auto node = detail::appendNode<ast::nodes::TypeAlias>(nodeStack, info, token);
+
+            nodeStack.push(node);
+            tokenStack.pop();
+
+            return ParserState::UsingName;
+        }
+
+        std::stringstream ss;
+        ss << type;
+
+        throw SyntaxError("Expected string token, found: " + ss.str() + " (" + token.token().to_string() + ")", token);
     }
 }}}

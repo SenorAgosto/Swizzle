b7cad25027e0835b7f3eb810c02c21271f33bb25
Austin Gilbert <ceretullis@gmail.com>
2017-05-28 18:30:34 -0500

    parser: states: implementing UsingReadEqualState

--- swizzle/src/parser/states/UsingReadEqualState.cpp
+++ swizzle/src/parser/states/UsingReadEqualState.cpp
@@ -1,14 +1,27 @@
 #include <swizzle/parser/states/UsingReadEqualState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
+#include <sstream>
+
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState UsingReadEqualState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack)
+    ParserState UsingReadEqualState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack& tokenStack)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if((type == lexer::TokenType::type) || (type == lexer::TokenType::string))
+        {
+            tokenStack.push(token);
+            return ParserState::UsingTypeRead;
+        }
+
+        std::stringstream ss;
+        ss << type;
+
+        throw SyntaxError("Expected type or type name, found " + ss.str() + " (" + token.token().to_string() + ")", token);
     }
 }}}
---
+++ swizzle/tests/unit_test/Parser-ReadEqualState-UT.cpp
@@ -0,0 +1,95 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <swizzle/ast/AbstractTreeEdgeVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVertexVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVisitorInterface.hpp>
+
+#include <swizzle/ast/AbstractSyntaxTree.hpp>
+#include <swizzle/ast/Node.hpp>
+#include <swizzle/parser/states/UsingReadEqualState.hpp>
+
+#include <swizzle/Exceptions.hpp>
+
+#include <array>
+#include <boost/filesystem.hpp>
+#include <iostream>
+
+namespace {
+
+    using namespace swizzle::ast;
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct UsingReadEqualStateFixture
+    {
+        UsingReadEqualStateFixture()
+        {
+            nodeStack.push(ast.root());
+        }
+
+        states::UsingReadEqualState state;
+
+        AbstractSyntaxTree ast;
+        NodeStack nodeStack;
+        TokenStack tokenStack;
+    };
+
+    TEST_FIXTURE(UsingReadEqualStateFixture, verifyConstruction)
+    {
+    }
+
+    struct WhenNextTokenIsType : public UsingReadEqualStateFixture
+    {
+        const Token token = Token("u8", 0, 2, TokenType::type);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsType, verifyConsume)
+    {
+        CHECK_EQUAL(1U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack);
+
+        CHECK_EQUAL(ParserState::UsingTypeRead, parserState);
+
+        REQUIRE CHECK_EQUAL(1U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(1U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsString : public UsingReadEqualStateFixture
+    {
+        const Token token = Token("PreviousType", 0, 12, TokenType::string);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsString, verifyConsume)
+    {
+        CHECK_EQUAL(1U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack);
+
+        CHECK_EQUAL(ParserState::UsingTypeRead, parserState);
+
+        REQUIRE CHECK_EQUAL(1U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(1U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsInvalid : public UsingReadEqualStateFixture
+    {
+        const Token token = Token(":", 0, 1, TokenType::colon);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsInvalid, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack), swizzle::SyntaxError);
+    }
+}

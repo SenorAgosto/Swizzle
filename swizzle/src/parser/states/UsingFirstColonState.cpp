d440c58e41eea20ce8fd805c92d70ec90d3fb602
Austin Gilbert <ceretullis@gmail.com>
2017-05-28 18:35:35 -0500

    parser: states: implementing UsingFirstColonState

--- swizzle/src/parser/states/UsingFirstColonState.cpp
+++ swizzle/src/parser/states/UsingFirstColonState.cpp
@@ -1,14 +1,26 @@
 #include <swizzle/parser/states/UsingFirstColonState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
+#include <sstream>
+
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState UsingFirstColonState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack)
+    ParserState UsingFirstColonState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack&)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::colon)
+        {
+            return ParserState::UsingSecondColon;
+        }
+
+        std::stringstream ss;
+        ss << type;
+
+        throw SyntaxError("Expected ':', found " + ss.str() + " (" + token.token().to_string() + ")", token);
     }
 }}}

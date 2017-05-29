03f8587be0c77ecfe86eebb458052b65b32b365b
Austin Gilbert <ceretullis@gmail.com>
2017-05-28 22:02:43 -0500

    parser: states: implementing EnumUnderlyingTypeState

--- swizzle/src/parser/states/EnumUnderlyingTypeState.cpp
+++ swizzle/src/parser/states/EnumUnderlyingTypeState.cpp
@@ -1,15 +1,27 @@
 #include <swizzle/parser/states/EnumUnderlyingTypeState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
+#include <sstream>
+
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState EnumUnderlyingTypeState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
+    ParserState EnumUnderlyingTypeState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack&, ParserStateContext&)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::l_brace)
+        {
+            return ParserState::EnumStartScope;
+        }
+
+        std::stringstream ss;
+        ss << type;
+
+        throw SyntaxError("Expected '{', found " + ss.str() + " (" + token.token().to_string() + ")", token);
     }
 }}}

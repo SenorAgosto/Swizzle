6705d31631740a83b227f5a96c674af66a79f9d1
Austin Gilbert <ceretullis@gmail.com>
2017-05-28 23:04:04 -0500

    ast: nodes: fixing initialization of EnumField, adding fields to store assigned values.

--- swizzle/src/ast/nodes/EnumField.cpp
+++ swizzle/src/ast/nodes/EnumField.cpp
@@ -3,24 +3,40 @@
 
 namespace swizzle { namespace ast { namespace nodes {
 
-    EnumField::EnumField(const lexer::TokenInfo& info, const lexer::TokenInfo& underlyingType)
+    EnumField::EnumField(const lexer::TokenInfo& name, const lexer::TokenInfo& underlyingType)
+        : name_(name)
+        , underlying_(underlyingType)
+        , value_(0)
+        , isNegative_(false)
     {
     }
 
     const lexer::TokenInfo& EnumField::name() const
     {
+        return name_;
     }
 
-    const lexer::TokenInfo& EnumField::underlyingType() const
+    const lexer::TokenInfo& EnumField::underlying() const
     {
+        return underlying_;
     }
 
-    void EnumField::value(const lexer::TokenInfo& val)
+    void EnumField::valueInfo(const lexer::TokenInfo& val)
     {
-        value_ = val;
+        valueInfo_ = val;
     }
 
-    const lexer::TokenInfo& EnumField::value() const
+    const lexer::TokenInfo& EnumField::valueInfo() const
+    {
+        return valueInfo_;
+    }
+
+    bool EnumField::isNegative() const
+    {
+        return isNegative_;
+    }
+
+    std::size_t EnumField::value() const
     {
         return value_;
     }

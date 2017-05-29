95b9608864d94dc086e8069917f49afc4447e228
Austin Gilbert <ceretullis@gmail.com>
2017-05-28 19:41:51 -0500

    parser: states: implementing UsingTypeReadState

--- swizzle/src/parser/states/UsingTypeReadState.cpp
+++ swizzle/src/parser/states/UsingTypeReadState.cpp
@@ -1,14 +1,52 @@
 #include <swizzle/parser/states/UsingTypeReadState.hpp>
 
+#include <swizzle/Exceptions.hpp>
+#include <swizzle/ast/nodes/TypeAlias.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
+#include <swizzle/parser/detail/CreateType.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/TokenStack.hpp>
+
+#include <swizzle/ast/AbstractTreeEdgeVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVertexVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVisitorInterface.hpp>
+
+#include <swizzle/parser/detail/NodeStackTopIs.hpp>
+#include <sstream>
 
 namespace swizzle { namespace parser { namespace states {
 
     ParserState UsingTypeReadState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::colon)
+        {
+            return ParserState::UsingFirstColon;
+        }
+
+        if(type == lexer::TokenType::end_statement)
+        {
+            const auto info = detail::createType(tokenStack);
+
+            if(detail::nodeStackTopIs<ast::nodes::TypeAlias>(nodeStack))
+            {
+                auto& top = static_cast<ast::nodes::TypeAlias&>(*nodeStack.top());
+                top.existingType(info);
+
+                nodeStack.pop();
+            }
+            else
+            {
+                throw ParserError("Internal parser error in UsingTypeReadState, expected TypeAlias on top of NodeStack.");
+            }
+
+            return ParserState::TranslationUnitMain;
+        }
+
+        std::stringstream ss;
+        ss << type;
+
+        throw SyntaxError("Expected ':' or ';', found " + ss.str() + " (" + token.token().to_string() + ")", token);
     }
 }}}
---
+++ swizzle/tests/unit_test/Parser-UsingReadType-UT.cpp
@@ -0,0 +1,128 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <swizzle/ast/AbstractTreeEdgeVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVertexVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVisitorInterface.hpp>
+
+#include <swizzle/ast/AbstractSyntaxTree.hpp>
+#include <swizzle/ast/Node.hpp>
+#include <swizzle/ast/nodes/Comment.hpp>
+#include <swizzle/ast/nodes/TypeAlias.hpp>
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <swizzle/parser/states/UsingTypeReadState.hpp>
+
+#include <swizzle/Exceptions.hpp>
+#include <boost/utility/string_view.hpp>
+
+namespace {
+
+    using namespace swizzle::ast;
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct UsingTypeReadStateFixture
+    {
+        UsingTypeReadStateFixture()
+        {
+            nodeStack.push(ast.root());
+
+            const auto node = swizzle::parser::detail::appendNode<nodes::TypeAlias>(nodeStack, TokenInfo(Token(), FileInfo("test.swizzle")), TokenInfo(Token(), FileInfo("test.swizzle")));
+            nodeStack.push(node);
+        }
+
+        states::UsingTypeReadState state;
+
+        AbstractSyntaxTree ast;
+        NodeStack nodeStack;
+        TokenStack tokenStack;
+    };
+
+    TEST_FIXTURE(UsingTypeReadStateFixture, verifyConstruction)
+    {
+    }
+
+    struct WhenNextTokenIsColon : public UsingTypeReadStateFixture
+    {
+        const Token token = Token(":", 0, 1, TokenType::colon);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsColon, verifyConsume)
+    {
+        CHECK_EQUAL(2U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack);
+
+        CHECK_EQUAL(ParserState::UsingFirstColon, parserState);
+
+        REQUIRE CHECK_EQUAL(2U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsEndStatement : public UsingTypeReadStateFixture
+    {
+        WhenNextTokenIsEndStatement()
+        {
+            tokenStack.emplace(Token(sv, 0, 3, TokenType::string), FileInfo("test.swizzle", LineInfo(1, 0), LineInfo(1, 4)));
+            tokenStack.emplace(Token(sv, 6, 3, TokenType::string), FileInfo("test.swizzle", LineInfo(1, 6), LineInfo(1, 9)));
+            tokenStack.emplace(Token(sv, 10, 6, TokenType::string), FileInfo("test.swizzle", LineInfo(1, 11), LineInfo(1, 17)));
+        }
+
+        const boost::string_view sv = "foo::bar::MyType";
+
+        const Token token = Token(";", 0, 1, TokenType::end_statement);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsEndStatement, verifyConsume)
+    {
+        CHECK_EQUAL(2U, nodeStack.size());
+        CHECK_EQUAL(3U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack);
+
+        CHECK_EQUAL(ParserState::TranslationUnitMain, parserState);
+
+        REQUIRE CHECK_EQUAL(1U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsInvalid : public UsingTypeReadStateFixture
+    {
+        const Token token = Token("=", 0, 1, TokenType::equal);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsInvalid, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack), swizzle::SyntaxError);
+    }
+
+    struct WhenTopOfNodeStackIsNotTypeAlias : public UsingTypeReadStateFixture
+    {
+        WhenTopOfNodeStackIsNotTypeAlias()
+        {
+            nodeStack.pop();
+
+            Node::smartptr node = new nodes::Comment(TokenInfo());
+            nodeStack.push(node);
+        }
+
+        const Token token = Token("=", 0, 1, TokenType::equal);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenTopOfNodeStackIsNotTypeAlias, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack), swizzle::ParserError);
+    }
+}

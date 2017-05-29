5b75d313c201c7e6ad2c057aee81177e0237bbb6
Austin Gilbert <ceretullis@gmail.com>
2017-05-28 21:15:40 -0500

    parser: states: implementing StartEnumState

--- swizzle/src/parser/states/StartEnumState.cpp
+++ swizzle/src/parser/states/StartEnumState.cpp
@@ -1,15 +1,34 @@
 #include <swizzle/parser/states/StartEnumState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
+#include <swizzle/ast/AbstractTreeEdgeVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVertexVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVisitorInterface.hpp>
+#include <swizzle/ast/nodes/Enum.hpp>
+
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <sstream>
+
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState StartEnumState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
+    ParserState StartEnumState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack&, ParserStateContext& context)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::string)
+        {
+            detail::appendNode<ast::nodes::Enum>(nodeStack, token, context.CurrentNamespace);
+            return ParserState::EnumName;
+        }
+
+        std::stringstream ss;
+        ss << type;
+
+        throw SyntaxError("Expected enum name, found " + ss.str() + " (" + token.token().to_string() + ")", token);
     }
 }}}

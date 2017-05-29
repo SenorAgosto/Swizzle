4a6178f4d8f0d1f767a51e6320976d018924c65b
Austin Gilbert <ceretullis@gmail.com>
2017-05-28 22:02:43 -0500

    parser: states: implementing EnumUnderlyingTypeState

---
+++ swizzle/tests/unit_test/Parser-EnumUnderlyingTypeState-UT.cpp
@@ -0,0 +1,78 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <swizzle/ast/AbstractTreeEdgeVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVertexVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVisitorInterface.hpp>
+
+#include <swizzle/ast/AbstractSyntaxTree.hpp>
+#include <swizzle/ast/Node.hpp>
+#include <swizzle/ast/nodes/Enum.hpp>
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <swizzle/parser/ParserStateContext.hpp>
+#include <swizzle/parser/states/EnumUnderlyingTypeState.hpp>
+
+#include <swizzle/Exceptions.hpp>
+
+namespace {
+
+    using namespace swizzle::ast;
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct EnumUnderlyingTypeStateFixture
+    {
+        EnumUnderlyingTypeStateFixture()
+        {
+            nodeStack.push(ast.root());
+
+            const auto node = swizzle::parser::detail::appendNode<nodes::Enum>(nodeStack, TokenInfo(Token("my_enum", 0, 7, TokenType::string), FileInfo("test.swizzle")), "my_namespace");
+            nodeStack.push(node);
+        }
+
+        states::EnumUnderlyingTypeState state;
+
+        AbstractSyntaxTree ast;
+
+        NodeStack nodeStack;
+        TokenStack tokenStack;
+        ParserStateContext context;
+    };
+
+    TEST_FIXTURE(EnumUnderlyingTypeStateFixture, verifyConstruction)
+    {
+    }
+
+    struct WhenNextTokenIsLeftBrace : public EnumUnderlyingTypeStateFixture
+    {
+        const Token token = Token("{", 0, 2, TokenType::l_brace);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsLeftBrace, verifyConsume)
+    {
+        CHECK_EQUAL(2U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::EnumStartScope, parserState);
+
+        REQUIRE CHECK_EQUAL(2U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsInvalid : public EnumUnderlyingTypeStateFixture
+    {
+        const Token token = Token(";", 0, 1, TokenType::end_statement);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsInvalid, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack, context), swizzle::SyntaxError);
+    }
+}

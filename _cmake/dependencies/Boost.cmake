f5f8ee0e71f5699c0d74830a733f867281511cfe
Austin Gilbert <ceretullis@gmail.com>
2017-05-29 14:39:39 -0500

    Disable boost auto-linking on platforms that support it.

--- _cmake/dependencies/Boost.cmake
+++ _cmake/dependencies/Boost.cmake
@@ -3,5 +3,8 @@ set(Boost_USE_STATIC_LIBS OFF)
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME OFF)
 
+add_definitions(-DBOOST_ALL_NO_LIB)
+add_definitions(${Boost_LIB_DIAGNOSTIC_DEFINITIONS})
+
 find_package(Boost COMPONENTS filesystem system REQUIRED)
 include_directories(${Boost_INCLUDE_DIR})

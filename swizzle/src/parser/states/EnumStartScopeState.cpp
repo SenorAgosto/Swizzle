2b56c61afcec4861d5a69ef46c8498329be85d9a
Austin Gilbert <ceretullis@gmail.com>
2017-05-28 22:50:33 -0500

    parser: states: implementing EnumStartScopeState

--- swizzle/src/parser/states/EnumStartScopeState.cpp
+++ swizzle/src/parser/states/EnumStartScopeState.cpp
@@ -1,15 +1,76 @@
 #include <swizzle/parser/states/EnumStartScopeState.hpp>
 
+#include <swizzle/Exceptions.hpp>
+#include <swizzle/ast/nodes/Comment.hpp>
+#include <swizzle/ast/nodes/Enum.hpp>
+#include <swizzle/ast/nodes/EnumField.hpp>
+#include <swizzle/ast/nodes/MultilineComment.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <swizzle/parser/detail/NodeStackTopIs.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
+#include <swizzle/ast/AbstractTreeEdgeVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVertexVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVisitorInterface.hpp>
+
+#include <sstream>
+
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState EnumStartScopeState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
+    ParserState EnumStartScopeState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack&, ParserStateContext&)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::comment)
+        {
+            detail::appendNode<ast::nodes::Comment>(nodeStack, token);
+            return ParserState::EnumStartScope;
+        }
+
+        if(type == lexer::TokenType::multiline_comment)
+        {
+            detail::appendNode<ast::nodes::MultilineComment>(nodeStack, token);
+            return ParserState::EnumStartScope;
+        }
+
+        if(type == lexer::TokenType::string)
+        {
+            if(detail::nodeStackTopIs<ast::nodes::Enum>(nodeStack))
+            {
+                auto& top = static_cast<ast::nodes::Enum&>(*nodeStack.top());
+                const auto node = detail::appendNode<ast::nodes::EnumField>(nodeStack, token, top.underlying());
+                nodeStack.push(node);
+
+                return ParserState::EnumField;
+            }
+
+            throw ParserError("Internal parser error, top of stack was not ast::nodes::Enum");
+        }
+
+        if(type == lexer::TokenType::r_brace)
+        {
+            if(detail::nodeStackTopIs<ast::nodes::Enum>(nodeStack))
+            {
+                auto& top = static_cast<ast::nodes::Enum&>(*nodeStack.top());
+                if(top.empty())
+                {
+                    throw SyntaxError("Enum must have fields, no fields declared in '" + top.name() + "'", token);
+                }
+
+                nodeStack.pop();
+
+                return ParserState::TranslationUnitMain;
+            }
+
+            throw ParserError("Internal parser error, top of stack was not ast::nodes::Enum");
+        }
+
+        std::stringstream ss;
+        ss << type;
+
+        throw SyntaxError("Expected enum field definition, found " + ss.str() + " (" + token.token().to_string() + ")", token);
     }
 }}}
---
+++ swizzle/tests/unit_test/Parser-EnumStartScopeState-UT.cpp
@@ -0,0 +1,200 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <swizzle/ast/AbstractTreeEdgeVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVertexVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVisitorInterface.hpp>
+
+#include <swizzle/ast/AbstractSyntaxTree.hpp>
+#include <swizzle/ast/Node.hpp>
+#include <swizzle/ast/nodes/Enum.hpp>
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <swizzle/parser/ParserStateContext.hpp>
+#include <swizzle/parser/states/EnumStartScopeState.hpp>
+
+#include <swizzle/Exceptions.hpp>
+
+namespace {
+
+    using namespace swizzle::ast;
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct EnumStartScopeStateFixture
+    {
+        EnumStartScopeStateFixture()
+        {
+            nodeStack.push(ast.root());
+
+            const auto node = swizzle::parser::detail::appendNode<nodes::Enum>(nodeStack, TokenInfo(Token("my_enum", 0, 7, TokenType::string), FileInfo("test.swizzle")), "my_namespace");
+            nodeStack.push(node);
+        }
+
+        states::EnumStartScopeState state;
+
+        AbstractSyntaxTree ast;
+
+        NodeStack nodeStack;
+        TokenStack tokenStack;
+        ParserStateContext context;
+    };
+
+    TEST_FIXTURE(EnumStartScopeStateFixture, verifyConstruction)
+    {
+    }
+
+    struct WhenNextTokenIsComment : public EnumStartScopeStateFixture
+    {
+        const Token token = Token("// comment", 0, 10, TokenType::comment);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsComment, verifyConsume)
+    {
+        CHECK_EQUAL(2U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::EnumStartScope, parserState);
+
+        REQUIRE CHECK_EQUAL(2U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+
+        // TODO: validate ast
+    }
+
+    struct WhenNextTokenIsMultilineComment : public EnumStartScopeStateFixture
+    {
+        const Token token = Token("// comment \\\n comment2", 0, 21, TokenType::multiline_comment);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsMultilineComment, verifyConsume)
+    {
+        CHECK_EQUAL(2U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::EnumStartScope, parserState);
+
+        REQUIRE CHECK_EQUAL(2U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+
+        // TODO: validate ast
+    }
+
+    struct WhenNextTokenIsFieldName : public EnumStartScopeStateFixture
+    {
+        const Token token = Token("field1", 0, 6, TokenType::string);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsFieldName, verifyConsume)
+    {
+        CHECK_EQUAL(2U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::EnumField, parserState);
+
+        REQUIRE CHECK_EQUAL(3U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsRightBrace : public EnumStartScopeStateFixture
+    {
+        WhenNextTokenIsRightBrace()
+        {
+            auto& top = dynamic_cast<nodes::Enum&>(*nodeStack.top());
+            top.append(new Node());
+        }
+
+        const Token token = Token("}", 0, 1, TokenType::r_brace);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsRightBrace, verifyConsume)
+    {
+        CHECK_EQUAL(2U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::TranslationUnitMain, parserState);
+
+        REQUIRE CHECK_EQUAL(1U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsInvalid : public EnumStartScopeStateFixture
+    {
+        const Token token = Token(";", 0, 1, TokenType::end_statement);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsInvalid, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack, context), swizzle::SyntaxError);
+    }
+
+    struct WhenNextTokenIsFieldNameAndTopOfStackIsNotEnum : public EnumStartScopeStateFixture
+    {
+        WhenNextTokenIsFieldNameAndTopOfStackIsNotEnum()
+        {
+            nodeStack.pop();
+        }
+
+        const Token token = Token("field1", 0, 6, TokenType::string);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsFieldNameAndTopOfStackIsNotEnum, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack, context), swizzle::ParserError);
+    }
+
+    struct WhenNextTokenIsRightBraceAndTopOfStackIsNotEnum : public EnumStartScopeStateFixture
+    {
+        WhenNextTokenIsRightBraceAndTopOfStackIsNotEnum()
+        {
+            nodeStack.pop();
+        }
+
+        const Token token = Token("}", 0, 1, TokenType::r_brace);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsRightBraceAndTopOfStackIsNotEnum, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack, context), swizzle::ParserError);
+    }
+
+    struct WhenNextTokenIsRightBraceButNoFieldsWereCreated : public EnumStartScopeStateFixture
+    {
+        const Token token = Token("}", 0, 1, TokenType::r_brace);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsRightBraceButNoFieldsWereCreated, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack, context), swizzle::SyntaxError);
+    }
+}

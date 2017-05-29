9c4650503ad742f33daf66baaf48016fb9380f45
Austin Gilbert <ceretullis@gmail.com>
2017-05-28 21:22:02 -0500

    parser: states: implementing EnumNameState

--- swizzle/src/parser/states/EnumNameState.cpp
+++ swizzle/src/parser/states/EnumNameState.cpp
@@ -1,15 +1,27 @@
 #include <swizzle/parser/states/EnumNameState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
+#include <sstream>
+
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState EnumNameState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
+    ParserState EnumNameState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack&, ParserStateContext&)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::colon)
+        {
+            return ParserState::EnumColonRead;
+        }
+
+        std::stringstream ss;
+        ss << type;
+
+        throw SyntaxError("Expected ':', found " + ss.str() + " (" + token.token().to_string() + ")", token);
     }
 }}}

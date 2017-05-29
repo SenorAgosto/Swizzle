39ce9e928df5c8819a4a86415e58706537036895
Austin Gilbert <ceretullis@gmail.com>
2017-05-29 16:35:12 -0500

    parser: states: implementing EnumFieldValueReadState

--- swizzle/src/parser/states/EnumFieldValueReadState.cpp
+++ swizzle/src/parser/states/EnumFieldValueReadState.cpp
@@ -1,15 +1,32 @@
 #include <swizzle/parser/states/EnumFieldValueReadState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
+#include <swizzle/ast/AbstractTreeEdgeVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVertexVisitorInterface.hpp>
+#include <swizzle/ast/AbstractTreeVisitorInterface.hpp>
+
+#include <sstream>
+
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState EnumFieldValueReadState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
+    ParserState EnumFieldValueReadState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack&, ParserStateContext&)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::comma)
+        {
+            nodeStack.pop();
+            return ParserState::EnumStartScope;
+        }
+
+        std::stringstream ss;
+        ss << type;
+
+        throw SyntaxError("Expected ',' found " + ss.str() + " (" + token.token().to_string() + ")", token);
     }
 }}}

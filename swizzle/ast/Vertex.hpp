95cece8cd0c57bde3aa132d1104762aa3dff0601
Austin Gilbert <ceretullis@gmail.com>
2017-05-29 17:19:42 -0500

    ast: eliminate ObjectGraph vertex.

--- swizzle/ast/Vertex.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <ObjectGraph/Vertex.hpp>
-#include <swizzle/ast/detail/AbstractTreeTraits.hpp>
-
-namespace swizzle { namespace ast {
-
-    using Vertex = ObjectGraph::VertexImpl<detail::AbstractTreeTraits>;
-}}

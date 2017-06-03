9a16802de97b19572d7c57776bf97c2708b599b4
Austin Gilbert <ceretullis@gmail.com>
2017-06-02 19:29:40 -0500

    parser: states: implementing BitfieldUnderlyingTypeState

--- swizzle/src/parser/states/BitfieldUnderlyingTypeState.cpp
+++ swizzle/src/parser/states/BitfieldUnderlyingTypeState.cpp
@@ -1,5 +1,6 @@
 #include <swizzle/parser/states/BitfieldUnderlyingTypeState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
@@ -7,10 +8,16 @@
 
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState BitfieldUnderlyingTypeState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
+    ParserState BitfieldUnderlyingTypeState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack&, ParserStateContext&)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::l_brace)
+        {
+            return ParserState::BitfieldStartScope;
+        }
+
+        throw SyntaxError("Expected '{'", token);
     }
 
 }}}

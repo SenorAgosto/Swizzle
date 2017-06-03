9c5e540f4a357ca59ef35843260a3675a5f1d753
Austin Gilbert <ceretullis@gmail.com>
2017-06-03 18:16:49 -0500

    ast: nodes: adding StructField node

---
+++ swizzle/src/ast/nodes/StructField.cpp
@@ -0,0 +1,87 @@
+#include <swizzle/ast/nodes/StructField.hpp>
+
+#include <swizzle/ast/VisitorInterface.hpp>
+#include <swizzle/Exceptions.hpp>
+
+#include <boost/lexical_cast.hpp>
+
+namespace swizzle { namespace ast { namespace nodes {
+
+    StructField::StructField()
+        : arraySize_(0)
+        , isConst_(false)
+    {
+    }
+
+    void StructField::name(const lexer::TokenInfo& name)
+    {
+        name_ = name;
+    }
+
+    const lexer::TokenInfo& StructField::name() const
+    {
+        return name_;
+    }
+
+    void StructField::type(const std::string& type)
+    {
+        type_ = type;
+    }
+
+    std::string StructField::type() const
+    {
+        return type_;
+    }
+
+    void StructField::setConst()
+    {
+        isConst_ = true;
+    }
+
+    bool StructField::isConst() const
+    {
+        return isConst_;
+    }
+
+    void StructField::makeArray(const lexer::TokenInfo& token)
+    {
+        try
+        {
+            arraySize_ = boost::lexical_cast<std::size_t>(token.token().value());
+        }
+        catch(const boost::bad_lexical_cast&)
+        {
+            throw SyntaxError("Expected positive numeric array size", token);
+        }
+    }
+
+    bool StructField::isArray() const
+    {
+        return arraySize_ != 0;
+    }
+
+    void StructField::makeVector(const std::string& sizeMember)
+    {
+        if(isArray())
+        {
+            throw ParserError("Internal parser error, struct field cannot be both array and vector.");
+        }
+
+        vectorOnField_ = sizeMember;
+    }
+
+    bool StructField::isVector() const
+    {
+        return !vectorOnField_.empty();
+    }
+
+    void StructField::accept(VisitorInterface& visitor)
+    {
+        visitor(*this);
+
+        for(auto child : children())
+        {
+            child->accept(visitor);
+        }
+    }
+}}}

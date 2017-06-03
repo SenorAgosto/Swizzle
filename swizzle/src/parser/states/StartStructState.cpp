293cf7141f0e403876c04d3835328d351b526af5
Austin Gilbert <ceretullis@gmail.com>
2017-06-03 14:37:54 -0500

    parser: states: implementing StartStructState

--- swizzle/src/parser/states/StartStructState.cpp
+++ swizzle/src/parser/states/StartStructState.cpp
@@ -1,6 +1,10 @@
 #include <swizzle/parser/states/StartStructState.hpp>
 
+#include <swizzle/ast/nodes/Struct.hpp>
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <swizzle/parser/detail/NodeStackTopIs.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
 #include <swizzle/parser/TokenStack.hpp>
@@ -9,7 +13,24 @@ namespace swizzle { namespace parser { namespace states {
 
     ParserState StartStructState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::string)
+        {
+            if(tokenStack.empty())
+            {
+                throw ParserError("Internal parser error, token stack empty. Expected struct keyword token info.");
+            }
+
+            auto& structKeyword = tokenStack.top();
+            auto node = detail::appendNode<ast::nodes::Struct>(nodeStack, structKeyword, token, context.CurrentNamespace);
+
+            nodeStack.push(node);
+            tokenStack.pop();
+
+            return ParserState::StructName;
+        }
+
+        throw SyntaxError("Expected struct name", token);
     }
 }}}

5cf673c0ae3d7039bb3aeb44406a9f539d05d21c
Austin Gilbert <ceretullis@gmail.com>
2017-06-02 21:54:37 -0500

    parser: states: implementing BitfieldSecondDotState

--- swizzle/src/parser/states/BitfieldSecondDotState.cpp
+++ swizzle/src/parser/states/BitfieldSecondDotState.cpp
@@ -1,15 +1,32 @@
 #include <swizzle/parser/states/BitfieldSecondDotState.hpp>
 
+#include <swizzle/ast/nodes/BitfieldField.hpp>
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
+#include <swizzle/parser/detail/NodeStackTopIs.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState BitfieldSecondDotState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
+    ParserState BitfieldSecondDotState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack&, ParserStateContext& context)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::numeric_literal)
+        {
+            if(detail::nodeStackTopIs<ast::nodes::BitfieldField>(nodeStack))
+            {
+                auto& top = static_cast<ast::nodes::BitfieldField&>(*nodeStack.top());
+                top.endBit(token, context);
+
+                return ParserState::BitfieldEndPosition;
+            }
+
+            throw ParserError("Internal parser error, top of node stack is not ast::nodes::BitfieldField");
+        }
+
+        throw SyntaxError("Expected bit position (numeric literal)", token);
     }
 }}}

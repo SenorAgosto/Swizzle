964e826a121600e90fa19d50f06c8b4a2c502042
Austin Gilbert <ceretullis@gmail.com>
2017-06-02 19:18:28 -0500

    Wrapping static variable in anonymous namespace to prevent linker errors.

--- swizzle/src/IsType.cpp
+++ swizzle/src/IsType.cpp
@@ -3,8 +3,10 @@
 
 namespace swizzle {
 
-    static const std::regex keywords("u8|i8|u16|i16|u32|i32|u64|i64|bitfield|f32|f64|variable_block", std::regex::optimize);
-
+    namespace {
+        static const std::regex keywords("u8|i8|u16|i16|u32|i32|u64|i64|bitfield|f32|f64|variable_block", std::regex::optimize);
+    }
+    
     bool IsType(const boost::string_view& token)
     {
         return std::regex_match(token.begin(), token.end(), keywords);

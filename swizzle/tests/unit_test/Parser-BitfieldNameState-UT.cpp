48dd1cb3764352c8c2ba548da58e6288995963c1
Austin Gilbert <ceretullis@gmail.com>
2017-06-02 19:02:10 -0500

    parser: states: implementing BitfieldNameState

---
+++ swizzle/tests/unit_test/Parser-BitfieldNameState-UT.cpp
@@ -0,0 +1,68 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <swizzle/ast/AbstractSyntaxTree.hpp>
+#include <swizzle/ast/Node.hpp>
+#include <swizzle/Exceptions.hpp>
+#include <swizzle/parser/ParserStateContext.hpp>
+#include <swizzle/parser/states/BitfieldNameState.hpp>
+
+namespace {
+
+    using namespace swizzle::ast;
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct BitfieldNameStateFixture
+    {
+        BitfieldNameStateFixture()
+        {
+            nodeStack.push(ast.root());
+        }
+
+        states::BitfieldNameState state;
+
+        AbstractSyntaxTree ast;
+
+        NodeStack nodeStack;
+        TokenStack tokenStack;
+        ParserStateContext context;
+    };
+
+    TEST_FIXTURE(BitfieldNameStateFixture, verifyConstruction)
+    {
+    }
+
+    struct WhenNextTokenIsColon : public BitfieldNameStateFixture
+    {
+        const Token token = Token(":", 0, 1, TokenType::colon);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsColon, verifyConsume)
+    {
+        CHECK_EQUAL(1U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::BitfieldColonRead, parserState);
+
+        REQUIRE CHECK_EQUAL(1U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsInvalid : public BitfieldNameStateFixture
+    {
+        const Token token = Token(";", 0, 1, TokenType::end_statement);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsInvalid, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack, context), swizzle::SyntaxError);
+    }
+}

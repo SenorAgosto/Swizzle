f55772b1bc09cd4a27bba5c69c2e32a8d4b26ceb
Austin Gilbert <ceretullis@gmail.com>
2017-06-05 18:42:14 -0500

    Adding a TypeCache member to ParserStateContext for holding user defined types

--- swizzle/parser/ParserStateContext.hpp
+++ swizzle/parser/ParserStateContext.hpp
@@ -1,14 +1,18 @@
 #pragma once
+#include <swizzle/ast/Node.hpp>
 #include <swizzle/types/EnumValue.hpp>
 #include <swizzle/types/EnumValueType.hpp>
 
 #include <cstddef>
 #include <string>
+#include <unordered_map>
 
 namespace swizzle { namespace parser {
 
     struct ParserStateContext
     {
+        std::unordered_map<std::string, ast::Node::smartptr> TypeCache;
+
         std::string CurrentNamespace;
         std::size_t CurrentBitfieldBit = 0;
         types::EnumValue CurrentEnumValue = types::EnumValue(types::EnumValueType(std::uint64_t(0)));

cb0bd00394a6f1e2fdbb2504c056c136ce202301
Austin Gilbert <ceretullis@gmail.com>
2017-06-06 14:50:09 -0500

    parser: states: implementing StructStartArrayState

--- swizzle/src/parser/states/StructStartArrayState.cpp
+++ swizzle/src/parser/states/StructStartArrayState.cpp
@@ -1,6 +1,9 @@
 #include <swizzle/parser/states/StructStartArrayState.hpp>
 
+#include <swizzle/ast/nodes/StructField.hpp>
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
+#include <swizzle/parser/detail/NodeStackTopIs.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
 #include <swizzle/parser/TokenStack.hpp>
@@ -9,7 +12,27 @@ namespace swizzle { namespace parser { namespace states {
 
     ParserState StructStartArrayState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::numeric_literal)
+        {
+            if(detail::nodeStackTopIs<ast::nodes::StructField>(nodeStack))
+            {
+                auto& top = static_cast<ast::nodes::StructField&>(*nodeStack.top());
+                top.makeArray(token);
+
+                return ParserState::StructArray;
+            }
+
+            throw ParserError("Internal parser error, top of node stack was not ast::nodes::StructField");
+        }
+
+        if(type == lexer::TokenType::string)
+        {
+            tokenStack.push(token);
+            return ParserState::StructVector;
+        }
+
+        throw SyntaxError("Expected array size or vector size member name", token);
     }
 }}}
---
+++ swizzle/tests/unit_test/Parser-StructStartArrayState-UT.cpp
@@ -0,0 +1,118 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <swizzle/ast/AbstractSyntaxTree.hpp>
+#include <swizzle/ast/Node.hpp>
+#include <swizzle/ast/nodes/Attribute.hpp>
+#include <swizzle/ast/nodes/Struct.hpp>
+#include <swizzle/ast/nodes/StructField.hpp>
+
+#include <swizzle/Exceptions.hpp>
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <swizzle/parser/ParserStateContext.hpp>
+#include <swizzle/parser/states/StructStartArrayState.hpp>
+
+namespace {
+
+    using namespace swizzle::ast;
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct StructStartArrayStateFixture
+    {
+        StructStartArrayStateFixture()
+        {
+            nodeStack.push(ast.root());
+
+            const auto structKeyword = TokenInfo(Token("struct", 0, 6, TokenType::keyword), FileInfo("test.swizzle"));
+            const auto name = TokenInfo(Token("MyStruct", 0, 8, TokenType::string), FileInfo("test.swizzle"));
+
+            auto node = detail::appendNode<nodes::Struct>(nodeStack, structKeyword, name, "my_namespace");
+            nodeStack.push(node);
+
+            node = detail::appendNode<nodes::StructField>(nodeStack);
+            nodeStack.push(node);
+        }
+
+        states::StructStartArrayState state;
+
+        AbstractSyntaxTree ast;
+
+        NodeStack nodeStack;
+        TokenStack tokenStack;
+        ParserStateContext context;
+    };
+
+    TEST_FIXTURE(StructStartArrayStateFixture, verifyConstruction)
+    {
+    }
+
+    struct WhenNextTokenIsNumericLiteral : public StructStartArrayStateFixture
+    {
+        const Token token = Token("10", 0, 2, TokenType::numeric_literal);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsNumericLiteral, verifyConsume)
+    {
+        CHECK_EQUAL(3U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::StructArray, parserState);
+
+        REQUIRE CHECK_EQUAL(3U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+
+        // TODO: check ast, field should be array type of size 10
+    }
+
+    struct WhenNextTokenIsNegativeNumericLiteral : public StructStartArrayStateFixture
+    {
+        const Token token = Token("-10", 0, 3, TokenType::numeric_literal);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsNegativeNumericLiteral, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack, context), swizzle::SyntaxError);
+    }
+
+    struct WhenNextTokenIsString : public StructStartArrayStateFixture
+    {
+        const Token token = Token("boyah", 0, 5, TokenType::string);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsString, verifyConsume)
+    {
+        CHECK_EQUAL(3U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::StructVector, parserState);
+
+        REQUIRE CHECK_EQUAL(3U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(1U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsInvalid : public StructStartArrayStateFixture
+    {
+        const Token token = Token(":", 0, 1, TokenType::colon);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsInvalid, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack, context), swizzle::SyntaxError);
+    }
+}

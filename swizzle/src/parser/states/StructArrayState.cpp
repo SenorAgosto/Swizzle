10fbba3362759bbc7e818581cf6b89cc58901037
Austin Gilbert <ceretullis@gmail.com>
2017-06-06 15:08:16 -0500

    parser: states: implementing StructArrayState

---
+++ swizzle/src/parser/states/StructArrayState.cpp
@@ -0,0 +1,22 @@
+#include <swizzle/parser/states/StructArrayState.hpp>
+
+#include <swizzle/Exceptions.hpp>
+#include <swizzle/lexer/TokenInfo.hpp>
+#include <swizzle/parser/NodeStack.hpp>
+#include <swizzle/parser/ParserStateContext.hpp>
+#include <swizzle/parser/TokenStack.hpp>
+
+namespace swizzle { namespace parser { namespace states {
+
+    ParserState StructArrayState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack&, ParserStateContext&)
+    {
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::r_bracket)
+        {
+            return ParserState::StructStartScope;
+        }
+
+        throw SyntaxError("Expected ']'", token);
+    }
+}}}
---
+++ swizzle/tests/unit_test/Parser-StructArrayState-UT.cpp
@@ -0,0 +1,82 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <swizzle/ast/AbstractSyntaxTree.hpp>
+#include <swizzle/ast/Node.hpp>
+#include <swizzle/ast/nodes/Attribute.hpp>
+#include <swizzle/ast/nodes/Struct.hpp>
+#include <swizzle/ast/nodes/StructField.hpp>
+
+#include <swizzle/Exceptions.hpp>
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <swizzle/parser/ParserStateContext.hpp>
+#include <swizzle/parser/states/StructArrayState.hpp>
+
+namespace {
+
+    using namespace swizzle::ast;
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct StructArrayStateFixture
+    {
+        StructArrayStateFixture()
+        {
+            nodeStack.push(ast.root());
+
+            const auto structKeyword = TokenInfo(Token("struct", 0, 6, TokenType::keyword), FileInfo("test.swizzle"));
+            const auto name = TokenInfo(Token("MyStruct", 0, 8, TokenType::string), FileInfo("test.swizzle"));
+
+            auto node = detail::appendNode<nodes::Struct>(nodeStack, structKeyword, name, "my_namespace");
+            nodeStack.push(node);
+
+            node = detail::appendNode<nodes::StructField>(nodeStack);
+            nodeStack.push(node);
+        }
+
+        states::StructArrayState state;
+
+        AbstractSyntaxTree ast;
+
+        NodeStack nodeStack;
+        TokenStack tokenStack;
+        ParserStateContext context;
+    };
+
+    TEST_FIXTURE(StructArrayStateFixture, verifyConstruction)
+    {
+    }
+
+    struct WhenNextTokenIsRightBracket : public StructArrayStateFixture
+    {
+        const Token token = Token("]", 0, 1, TokenType::r_bracket);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsRightBracket, verifyConsume)
+    {
+        CHECK_EQUAL(3U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(info, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::StructStartScope, parserState);
+
+        REQUIRE CHECK_EQUAL(3U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsInvalid : public StructArrayStateFixture
+    {
+        const Token token = Token(":", 0, 1, TokenType::colon);
+        const FileInfo fileInfo = FileInfo("test.swizzle");
+
+        const TokenInfo info = TokenInfo(token, fileInfo);
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsInvalid, verifyConsume)
+    {
+        CHECK_THROW(state.consume(info, nodeStack, tokenStack, context), swizzle::SyntaxError);
+    }
+}

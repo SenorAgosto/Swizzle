84c33c922246f9c6518fb13bb2eb99f62c447e4b
Austin Gilbert <ceretullis@gmail.com>
2017-06-10 17:42:27 -0500

    ast: matchers: adding matcher for matching field names in structs and bitfields.

---
+++ swizzle/src/ast/matchers/HasFieldNamed.cpp
@@ -0,0 +1,62 @@
+#include <swizzle/ast/matchers/HasFieldNamed.hpp>
+#include <swizzle/ast/DefaultVisitor.hpp>
+
+#include <swizzle/ast/nodes/BitfieldField.hpp>
+#include <swizzle/ast/nodes/StructField.hpp>
+
+namespace swizzle { namespace ast { namespace matchers {
+
+    namespace {
+        class FieldVisitor : public DefaultVisitor
+        {
+        public:
+            FieldVisitor(const std::string& fieldName)
+                : fieldName_(fieldName)
+                , found_(false)
+            {
+            }
+
+            void operator()(nodes::BitfieldField& node) override
+            {
+                found_ = node.name().token().value() == fieldName_;
+            }
+
+            void operator()(nodes::StructField& node) override
+            {
+                found_ = node.name().token().value() == fieldName_;
+            }
+
+            bool found() const
+            {
+                return found_;
+            }
+
+        private:
+            std::string fieldName_;
+            bool found_;
+        };
+    }
+
+    HasFieldNamed::HasFieldNamed(const std::string& name)
+        : name_(name)
+    {
+    }
+
+    bool HasFieldNamed::evaluate(Node::smartptr node)
+    {
+        FieldVisitor v(name_);
+
+        for(const auto child : node->children())
+        {
+            child->accept(v);
+
+            if(v.found())
+            {
+                return true;
+            }
+        }
+
+        return v.found();
+    }
+}}}
+

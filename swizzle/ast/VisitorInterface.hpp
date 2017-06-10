044126ae159142459714dd67057803d0664ec156
Austin Gilbert <ceretullis@gmail.com>
2017-06-09 21:18:13 -0500

    ast: adding newly added nodes to the ast::VisitorInterface

--- swizzle/ast/VisitorInterface.hpp
+++ swizzle/ast/VisitorInterface.hpp
@@ -3,15 +3,20 @@
 namespace swizzle { namespace ast { namespace nodes {
     class Attribute;
     class AttributeBlock;
+    class Bitfield;
+    class BitfieldField;
     class CharLiteral;
     class Comment;
     class Enum;
     class EnumField;
+    class FieldLabel;
     class HexLiteral;
     class Import;
     class MultilineComment;
     class NumericLiteral;
     class StringLiteral;
+    class Struct;
+    class StructField;
     class TypeAlias;
 }}}
 
@@ -25,15 +30,20 @@ namespace swizzle { namespace ast {
 
         virtual void operator()(nodes::Attribute& node) = 0;
         virtual void operator()(nodes::AttributeBlock& node) = 0;
+        virtual void operator()(nodes::Bitfield& node) = 0;
+        virtual void operator()(nodes::BitfieldField& node) = 0;
         virtual void operator()(nodes::CharLiteral& node) = 0;
         virtual void operator()(nodes::Comment& node) = 0;
         virtual void operator()(nodes::Enum& node) = 0;
         virtual void operator()(nodes::EnumField& node) = 0;
+        virtual void operator()(nodes::FieldLabel& node) = 0;
         virtual void operator()(nodes::HexLiteral& node) = 0;
         virtual void operator()(nodes::Import& node) = 0;
         virtual void operator()(nodes::MultilineComment& node) = 0;
         virtual void operator()(nodes::NumericLiteral& node) = 0;
         virtual void operator()(nodes::StringLiteral& node) = 0;
+        virtual void operator()(nodes::Struct& node) = 0;
+        virtual void operator()(nodes::StructField& node) = 0;
         virtual void operator()(nodes::TypeAlias& node) = 0;
     };
 }}

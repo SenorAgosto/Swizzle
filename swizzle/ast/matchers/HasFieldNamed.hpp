49f2951a9185e8b7738d1cd5c185916e1e99f207
Austin Gilbert <ceretullis@gmail.com>
2017-06-10 17:42:27 -0500

    ast: matchers: adding matcher for matching field names in structs and bitfields.

---
+++ swizzle/ast/matchers/HasFieldNamed.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <swizzle/ast/MatchRule.hpp>
+#include <string>
+
+namespace swizzle { namespace ast { namespace matchers {
+
+    class HasFieldNamed : public MatchRule
+    {
+    public:
+        HasFieldNamed(const std::string& name);
+
+        bool evaluate(Node::smartptr node) override;
+
+    private:
+        std::string name_;
+    };
+}}}
+
+namespace swizzle { namespace ast { namespace matchers { namespace fluent {
+
+    template<class Matcher>
+    class HasFieldNamed
+    {
+    public:
+        HasFieldNamed(Matcher& matcher)
+            : matcher_(matcher)
+        {
+        }
+
+        Matcher& hasFieldNamed(const std::string& name)
+        {
+            matcher_.template append<swizzle::ast::matchers::HasFieldNamed>(name);
+            return matcher_;
+        }
+
+    private:
+        Matcher& matcher_;
+    };
+}}}}

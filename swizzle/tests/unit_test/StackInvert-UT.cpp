4de01e69611c287a08891380b92dfbfba42ddf47
Austin Gilbert <ceretullis@gmail.com>
2017-06-16 22:53:25 -0500

    Extracting some common code into a reusable method.

---
+++ swizzle/tests/unit_test/StackInvert-UT.cpp
@@ -0,0 +1,29 @@
+#include "./ut_support/UnitTestSupport.hpp"
+#include <swizzle/parser/utils/StackInvert.hpp>
+
+namespace {
+
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct InvertFixture
+    {
+        InvertFixture()
+        {
+            tokenStack.emplace(TokenInfo(Token("foo", 0, 3, TokenType::string), FileInfo("test.swizzle")));
+            tokenStack.emplace(TokenInfo(Token("bar", 0, 3, TokenType::string), FileInfo("test.swizzle")));
+            tokenStack.emplace(TokenInfo(Token("baz", 0, 3, TokenType::string), FileInfo("test.swizzle")));
+        }
+
+        TokenStack tokenStack;
+    };
+
+    TEST_FIXTURE(InvertFixture, verifyInvert)
+    {
+        TokenStack stack = utils::stack::invert(tokenStack);
+
+        CHECK_EQUAL("foo", stack.top().token().to_string()); stack.pop();
+        CHECK_EQUAL("bar", stack.top().token().to_string()); stack.pop();
+        CHECK_EQUAL("baz", stack.top().token().to_string()); stack.pop();
+    }
+}

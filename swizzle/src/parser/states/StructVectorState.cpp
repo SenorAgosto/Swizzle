2782c6b38f672809be335f95a802ff4d387fcc3b
Austin Gilbert <ceretullis@gmail.com>
2017-07-01 12:43:44 -0500

    parser: states: making loop variable const correct.

--- swizzle/src/parser/states/StructVectorState.cpp
+++ swizzle/src/parser/states/StructVectorState.cpp
@@ -1,6 +1,11 @@
 #include <swizzle/parser/states/StructVectorState.hpp>
 
+#include <swizzle/ast/nodes/StructField.hpp>
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
+#include <swizzle/parser/detail/CreateMember.hpp>
+#include <swizzle/parser/detail/NodeStackTopIs.hpp>
+#include <swizzle/parser/detail/ValidateVectorSizeMember.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
 #include <swizzle/parser/TokenStack.hpp>
@@ -9,7 +14,32 @@ namespace swizzle { namespace parser { namespace states {
 
     ParserState StructVectorState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::dot)
+        {
+            return ParserState::StructVectorNestedOnMember;
+        }
+
+        if(type == lexer::TokenType::r_bracket)
+        {
+            if(detail::nodeStackTopIs<ast::nodes::StructField>(nodeStack))
+            {
+                detail::validateVectorSizeMember(token, nodeStack, tokenStack, context);
+                const auto member = detail::createMember(token, nodeStack, tokenStack, context);
+
+                auto& top = static_cast<ast::nodes::StructField&>(*nodeStack.top());
+                top.makeVector(member);
+
+                nodeStack.pop();
+                return ParserState::StructStartScope;
+            }
+            else
+            {
+                throw ParserError("Internal parser error, top of node stack was not ast::nodes::StructField");
+            }
+        }
+
+        throw SyntaxError("Expected ']' or '.'" , token);
     }
 }}}

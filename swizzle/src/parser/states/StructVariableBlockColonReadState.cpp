c5905861cafeebb41e0769d8d475d122c4c0f597
Austin Gilbert <ceretullis@gmail.com>
2017-07-03 17:37:45 -0500

    parser: states: implementing StructVariableBlockColonReadState

--- swizzle/src/parser/states/StructVariableBlockColonReadState.cpp
+++ swizzle/src/parser/states/StructVariableBlockColonReadState.cpp
@@ -1,5 +1,6 @@
 #include <swizzle/parser/states/StructVariableBlockColonReadState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
@@ -7,9 +8,15 @@
 
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState StructVariableBlockColonReadState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
+    ParserState StructVariableBlockColonReadState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack& tokenStack, ParserStateContext&)
     {
-        // TODO: implement
-        return ParserState::Init;
-    }    
+        const auto type = token.token().type();
+        if(type == lexer::TokenType::string)
+        {
+            tokenStack.push(token);
+            return ParserState::StructVariableBlockOnField;
+        }
+
+        throw SyntaxError("Expected field name of type string", token);
+    }
 }}}

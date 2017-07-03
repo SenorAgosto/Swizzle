673c9a6e643a33abf09b76ae1a257e218fa08b4e
Austin Gilbert <ceretullis@gmail.com>
2017-07-02 21:15:53 -0500

    parser: states: implementing StructStartVariableBlockState.

--- swizzle/src/parser/states/StructStartVariableBlockState.cpp
+++ swizzle/src/parser/states/StructStartVariableBlockState.cpp
@@ -1,5 +1,6 @@
 #include <swizzle/parser/states/StructStartVariableBlockState.hpp>
 
+#include <swizzle/Exceptions.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
@@ -7,9 +8,14 @@
 
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState StructStartVariableBlockState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
+    ParserState StructStartVariableBlockState::consume(const lexer::TokenInfo& token, NodeStack&, TokenStack&, ParserStateContext&)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+        if(type == lexer::TokenType::colon)
+        {
+            return ParserState::StructVariableBlockColonRead;
+        }
+
+        throw SyntaxError("Expected ':'", token);
     }
 }}}

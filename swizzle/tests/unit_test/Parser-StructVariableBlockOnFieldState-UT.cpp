abd8fa0f10d7f6cf1ffbd2f22a0777ed28dc1e64
Austin Gilbert <ceretullis@gmail.com>
2017-07-07 21:59:21 -0500

    parser: states: implementing StructVariableBlockOnFieldState

---
+++ swizzle/tests/unit_test/Parser-StructVariableBlockOnFieldState-UT.cpp
@@ -0,0 +1,124 @@
+#include "./ut_support/UnitTestSupport.hpp"
+#include <swizzle/parser/states/StructVariableBlockOnFieldState.hpp>
+
+#include <swizzle/ast/AbstractSyntaxTree.hpp>
+#include <swizzle/ast/Node.hpp>
+
+#include <swizzle/Exceptions.hpp>
+#include <swizzle/ast/nodes/Struct.hpp>
+#include <swizzle/ast/nodes/StructField.hpp>
+#include <swizzle/ast/nodes/VariableBlock.hpp>
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <swizzle/parser/ParserStateContext.hpp>
+
+namespace{
+    using namespace swizzle::ast;
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct StructVariableBlockOnFieldStateFixture
+    {
+        StructVariableBlockOnFieldStateFixture()
+        {
+            nodeStack.push(ast.root());
+
+            context.CurrentNamespace = "my_namespace";
+
+            const auto info = TokenInfo(Token("struct", 0, 6, TokenType::keyword), FileInfo("test.swizzle"));
+            const auto name = TokenInfo(Token("MyStruct", 0, 8, TokenType::string), FileInfo("test.swizzle"));
+            const auto node = detail::appendNode<nodes::Struct>(nodeStack, info, name, "my_namespace");
+            nodeStack.push(node);
+
+            context.TypeCache["my_namespace::MyStruct"] = node;
+        }
+
+        states::StructVariableBlockOnFieldState state;
+        AbstractSyntaxTree ast;
+
+        NodeStack nodeStack;
+        TokenStack tokenStack;
+        ParserStateContext context;
+    };
+
+    TEST_FIXTURE(StructVariableBlockOnFieldStateFixture, verifyConstruction)
+    {
+    }
+
+    struct WhenNextTokenIsDot : public StructVariableBlockOnFieldStateFixture
+    {
+        const TokenInfo token = TokenInfo(Token(".", 0, 1, TokenType::dot), FileInfo("test.swizzle"));
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsDot, verifyConsume)
+    {
+        CHECK_EQUAL(2U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(token, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::StructVariableBlockOnNestedField, parserState);
+        CHECK_EQUAL(2U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsLeftBrace : public StructVariableBlockOnFieldStateFixture
+    {
+        WhenNextTokenIsLeftBrace()
+        {
+            auto node = detail::appendNode<nodes::StructField>(nodeStack);
+            auto& field = static_cast<nodes::StructField&>(*node);
+            field.name(TokenInfo(Token("field1", 0, 6, TokenType::string), FileInfo("test.swizzle")));
+            field.type("u8");
+
+            const TokenInfo info = TokenInfo(Token("variable_block", 0, 14, TokenType::keyword), FileInfo("test.swizzle"));
+            node = detail::appendNode<nodes::VariableBlock>(nodeStack, info);
+            nodeStack.push(node);
+
+            tokenStack.push(TokenInfo(Token("field1", 0, 6, TokenType::string), FileInfo("test.swizzle")));
+        }
+
+        const TokenInfo token = TokenInfo(Token("{", 0, 1, TokenType::l_brace), FileInfo("test.swizzle"));
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsLeftBrace, verifyConsume)
+    {
+        CHECK_EQUAL(3U, nodeStack.size());
+        CHECK_EQUAL(1U, tokenStack.size());
+
+        const auto parserState = state.consume(token, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::StructVariableBlockBeginCases, parserState);
+        CHECK_EQUAL(3U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+    }
+
+    struct WhenNextTokenIsLeftBraceAndVariableBlockFieldIsNotDefined : public StructVariableBlockOnFieldStateFixture
+    {
+        WhenNextTokenIsLeftBraceAndVariableBlockFieldIsNotDefined()
+        {
+            const TokenInfo info = TokenInfo(Token("variable_block", 0, 14, TokenType::keyword), FileInfo("test.swizzle"));
+            const auto node = detail::appendNode<nodes::VariableBlock>(nodeStack, info);
+            nodeStack.push(node);
+
+            tokenStack.push(TokenInfo(Token("field1", 0, 6, TokenType::string), FileInfo("test.swizzle")));
+        }
+
+        const TokenInfo token = TokenInfo(Token("{", 0, 1, TokenType::l_brace), FileInfo("test.swizzle"));
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsLeftBraceAndVariableBlockFieldIsNotDefined, verifyConsume)
+    {
+        CHECK_THROW(state.consume(token, nodeStack, tokenStack, context), swizzle::ParserError);
+    }
+
+    struct WhenNextTokenIsLeftBraceAndTokenStackIsEmpty : public StructVariableBlockOnFieldStateFixture
+    {
+        const TokenInfo token = TokenInfo(Token("{", 0, 1, TokenType::l_brace), FileInfo("test.swizzle"));
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsLeftBraceAndTokenStackIsEmpty, verifyConsume)
+    {
+        CHECK_THROW(state.consume(token, nodeStack, tokenStack, context), swizzle::ParserError);
+    }
+
+}

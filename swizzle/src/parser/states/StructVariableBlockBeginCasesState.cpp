ae1bcef23fab1c013ea1a2835af1efd41ceba2c9
Austin Gilbert <ceretullis@gmail.com>
2017-07-08 20:40:31 -0500

    parser: states: implementing StructVariableBlockBeginCasesState

--- swizzle/src/parser/states/StructVariableBlockBeginCasesState.cpp
+++ swizzle/src/parser/states/StructVariableBlockBeginCasesState.cpp
@@ -1,15 +1,50 @@
 #include <swizzle/parser/states/StructVariableBlockBeginCasesState.hpp>
 
+#include <swizzle/Exceptions.hpp>
+#include <swizzle/ast/nodes/Comment.hpp>
+#include <swizzle/ast/nodes/MultilineComment.hpp>
+#include <swizzle/ast/nodes/VariableBlockCase.hpp>
 #include <swizzle/lexer/TokenInfo.hpp>
+#include <swizzle/parser/detail/AppendNode.hpp>
 #include <swizzle/parser/NodeStack.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
 #include <swizzle/parser/TokenStack.hpp>
 
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState StructVariableBlockBeginCasesState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext& context)
+    ParserState StructVariableBlockBeginCasesState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack&, ParserStateContext&)
     {
-        // TODO: implement
-        return ParserState::Init;
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::comment)
+        {
+            detail::appendNode<ast::nodes::Comment>(nodeStack, token);
+            return ParserState::StructVariableBlockBeginCases;
+        }
+
+        if(type == lexer::TokenType::multiline_comment)
+        {
+            detail::appendNode<ast::nodes::MultilineComment>(nodeStack, token);
+            return ParserState::StructVariableBlockBeginCases;
+        }
+
+        if((type == lexer::TokenType::keyword) && (token.token().value() == "case"))
+        {
+            detail::appendNode<ast::nodes::VariableBlockCase>(nodeStack);
+            return ParserState::StructVariableBlockCaseValue;
+        }
+
+        if(type == lexer::TokenType::r_brace)
+        {
+            const auto& top = nodeStack.top();
+            if(top->empty())
+            {
+                throw SyntaxError("Expected variable block cases", " empty variable block", token.fileInfo());
+            }
+
+            return ParserState::StructStartScope;
+        }
+
+        throw SyntaxError("Expected 'case' or '}'", token);
     }    
 }}}
---
+++ swizzle/tests/unit_test/Parser-StructVariableBlockBeginCasesState-UT.cpp
@@ -0,0 +1,140 @@
+#include "./ut_support/UnitTestSupport.hpp"
+
+#include <swizzle/ast/AbstractSyntaxTree.hpp>
+#include <swizzle/ast/Node.hpp>
+#include <swizzle/ast/nodes/Comment.hpp>
+#include <swizzle/ast/nodes/MultilineComment.hpp>
+#include <swizzle/ast/nodes/VariableBlock.hpp>
+#include <swizzle/ast/nodes/VariableBlockCase.hpp>
+
+#include <swizzle/Exceptions.hpp>
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <swizzle/parser/ParserStateContext.hpp>
+#include <swizzle/parser/states/StructVariableBlockBeginCasesState.hpp>
+
+namespace {
+
+    using namespace swizzle::ast;
+    using namespace swizzle::lexer;
+    using namespace swizzle::parser;
+
+    struct StructVariableBlockBeginCasesStateFixture
+    {
+        StructVariableBlockBeginCasesStateFixture()
+        {
+            nodeStack.push(ast.root());
+        }
+
+        states::StructVariableBlockBeginCasesState state;
+
+        AbstractSyntaxTree ast;
+
+        NodeStack nodeStack;
+        TokenStack tokenStack;
+        ParserStateContext context;
+    };
+
+    TEST_FIXTURE(StructVariableBlockBeginCasesStateFixture, verifyConstruction)
+    {
+    }
+
+    struct WhenNextTokenIsComment : public StructVariableBlockBeginCasesStateFixture
+    {
+        const TokenInfo token = TokenInfo(Token("// comment", 0, 10, TokenType::comment), FileInfo("test.swizzle"));
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsComment, verifyConsume)
+    {
+        CHECK_EQUAL(1U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(token, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::StructVariableBlockBeginCases, parserState);
+
+        REQUIRE CHECK_EQUAL(1U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+
+        // TODO: validate AST
+    }
+
+    struct WhenNextTokenIsMultilineComment : public StructVariableBlockBeginCasesStateFixture
+    {
+        const TokenInfo token = TokenInfo(Token("// comment", 0, 10, TokenType::multiline_comment), FileInfo("test.swizzle"));
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsMultilineComment, verifyConsume)
+    {
+        CHECK_EQUAL(1U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(token, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::StructVariableBlockBeginCases, parserState);
+
+        REQUIRE CHECK_EQUAL(1U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+
+        // TODO: validate AST
+    }
+
+    struct WhenNextTokenIsCase : public StructVariableBlockBeginCasesStateFixture
+    {
+        const TokenInfo token = TokenInfo(Token("case", 0, 4, TokenType::keyword), FileInfo("test.swizzle"));
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsCase, verifyConsume)
+    {
+        CHECK_EQUAL(1U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(token, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::StructVariableBlockCaseValue, parserState);
+
+        REQUIRE CHECK_EQUAL(1U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+
+        // TODO: validate AST
+    }
+
+    struct WhenNextTokenIsRightBrace : public StructVariableBlockBeginCasesStateFixture
+    {
+        WhenNextTokenIsRightBrace()
+        {
+            const auto info = TokenInfo(Token("variable_block", 0, 14, TokenType::type), FileInfo("test.swizzle"));
+            const auto node = detail::appendNode<nodes::VariableBlock>(nodeStack, info);
+
+            nodeStack.push(node);
+
+            detail::appendNode<nodes::VariableBlockCase>(nodeStack);
+        }
+
+        const TokenInfo token = TokenInfo(Token("}", 0, 1, TokenType::r_brace), FileInfo("test.swizzle"));
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsRightBrace, verifyConsume)
+    {
+        CHECK_EQUAL(2U, nodeStack.size());
+        CHECK_EQUAL(0U, tokenStack.size());
+
+        const auto parserState = state.consume(token, nodeStack, tokenStack, context);
+
+        CHECK_EQUAL(ParserState::StructStartScope, parserState);
+
+        REQUIRE CHECK_EQUAL(2U, nodeStack.size());
+        REQUIRE CHECK_EQUAL(0U, tokenStack.size());
+
+        // TODO: validate AST
+    }
+
+    struct WhenNextTokenIsRightBraceAndVariableBlockIsEmpty : public StructVariableBlockBeginCasesStateFixture
+    {
+        const TokenInfo token = TokenInfo(Token("}", 0, 1, TokenType::r_brace), FileInfo("test.swizzle"));
+    };
+
+    TEST_FIXTURE(WhenNextTokenIsRightBraceAndVariableBlockIsEmpty, verifyConsume)
+    {
+        CHECK_THROW(state.consume(token, nodeStack, tokenStack, context), swizzle::SyntaxError);
+    }
+}

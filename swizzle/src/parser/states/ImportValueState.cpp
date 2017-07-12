b00056c10020d43ca92003f433c7952511132779
Austin Gilbert <ceretullis@gmail.com>
2017-07-11 22:44:31 -0500

    parser: states: removing callback from ImportValueStates, will implement a pre-parse step which will topo-sort header inclusion graph.

---
+++ swizzle/src/parser/states/ImportValueState.cpp
@@ -0,0 +1,37 @@
+#include <swizzle/parser/states/ImportValueState.hpp>
+
+#include <swizzle/Exceptions.hpp>
+#include <swizzle/ast/nodes/Import.hpp>
+#include <swizzle/lexer/TokenInfo.hpp>
+#include <swizzle/parser/NodeStack.hpp>
+#include <swizzle/parser/TokenStack.hpp>
+
+#include <swizzle/parser/detail/AppendNode.hpp>
+#include <swizzle/parser/detail/CreateImportPath.hpp>
+#include <swizzle/parser/detail/ValidateImportPath.hpp>
+
+namespace swizzle { namespace parser { namespace states {
+
+    ParserState ImportValueState::consume(const lexer::TokenInfo& token, NodeStack& nodeStack, TokenStack& tokenStack, ParserStateContext&)
+    {
+        const auto type = token.token().type();
+
+        if(type == lexer::TokenType::colon)
+        {
+            return ParserState::ImportFirstColon;
+        }
+
+        if(type == lexer::TokenType::end_statement)
+        {
+            const boost::filesystem::path import = detail::createImportPath(tokenStack);
+            detail::validateImportPath(import);
+
+            detail::appendNode<ast::nodes::Import>(nodeStack, token, import);
+
+            return ParserState::Init;
+        }
+
+        throw SyntaxError("Expected ; or :, found '" + token.token().to_string() + "'", token);
+    }
+
+}}}

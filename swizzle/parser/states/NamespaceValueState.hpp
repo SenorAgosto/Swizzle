f3039265d7fbf0af647fe80d5e5620d2eeef5522
Austin Gilbert <ceretullis@gmail.com>
2017-07-15 15:04:00 -0500

    parser: states: removing callback from NamespaceValueState

--- swizzle/parser/states/NamespaceValueState.hpp
+++ swizzle/parser/states/NamespaceValueState.hpp
@@ -1,47 +1,12 @@
 #pragma once 
 #include <swizzle/parser/ParserStateInterface.hpp>
 
-#include <swizzle/Exceptions.hpp>
-#include <swizzle/lexer/TokenInfo.hpp>
-#include <swizzle/parser/NodeStack.hpp>
-#include <swizzle/parser/TokenStack.hpp>
-
-#include <swizzle/parser/detail/CreateNamespace.hpp>
 
 namespace swizzle { namespace parser { namespace states {
 
-    template<class OnNamespaceCallback>
     class NamespaceValueState : public ParserStateInterface
     {
     public:
-        NamespaceValueState(OnNamespaceCallback callback)
-            : OnNamespace(callback)
-        {
-        }
-
-        ParserState consume(const lexer::TokenInfo& token, NodeStack&, TokenStack& tokenStack, ParserStateContext& context) override
-        {
-            const auto type = token.token().type();
-
-            if(type == lexer::TokenType::colon)
-            {
-                return ParserState::NamespaceFirstColon;
-            }
-
-            if(type == lexer::TokenType::end_statement)
-            {
-                const auto nameSpace = detail::createNamespace(tokenStack);
-                context.CurrentNamespace = nameSpace;
-
-                OnNamespace(nameSpace);
-
-                return ParserState::TranslationUnitMain;
-            }
-
-            throw SyntaxError("Expected : or ; but found '" + token.token().to_string() + "'", token);
-        }
-
-    private:
-        OnNamespaceCallback OnNamespace;
+        ParserState consume(const lexer::TokenInfo& token, NodeStack&, TokenStack& tokenStack, ParserStateContext& context) override;
     };
 }}}

af040ad231bcb59e14d14689fc8d70bdbfbb6a31
Austin Gilbert <ceretullis@gmail.com>
2017-07-16 16:49:45 -0500

    bug fix: SetValue() needs to know if we're reading a hex stream or not.

--- swizzle/types/SetValue.hpp
+++ swizzle/types/SetValue.hpp
@@ -4,46 +4,47 @@
 
 namespace swizzle { namespace types {
 
+    template<bool isHex = false>
     EnumValueType setValue(const boost::string_view& underlying, const boost::string_view& value)
     {
         if(underlying == "u8")
         {
-            return readAs<std::uint8_t>(value);
+            return readAsU8<isHex>(value);
         }
 
         if(underlying == "i8")
         {
-            return readAs<std::int8_t>(value);
+            return readAsI8<isHex>(value);
         }
 
         if(underlying == "u16")
         {
-            return readAs<std::uint16_t>(value);
+            return readAs<std::uint16_t, isHex>(value);
         }
 
         if(underlying == "i16")
         {
-            return readAs<std::int16_t>(value);
+            return readAs<std::int16_t, isHex>(value);
         }
 
         if(underlying == "u32")
         {
-            return readAs<std::uint32_t>(value);
+            return readAs<std::uint32_t, isHex>(value);
         }
 
         if(underlying == "i32")
         {
-            return readAs<std::int32_t>(value);
+            return readAs<std::int32_t, isHex>(value);
         }
 
         if(underlying == "u64")
         {
-            return readAs<std::uint64_t>(value);
+            return readAs<std::uint64_t, isHex>(value);
         }
 
         if(underlying == "i64")
         {
-            return readAs<std::int64_t>(value);
+            return readAs<std::int64_t, isHex>(value);
         }
 
         throw ParserError("Encountered unknown enum type:" + underlying.to_string());

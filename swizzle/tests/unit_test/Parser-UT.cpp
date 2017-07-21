bc0e40faa2c09286d754f729ea04df8eadac2907
Austin Gilbert <ceretullis@gmail.com>
2017-07-20 23:05:34 -0500

    unit tests: implementing unit test coverage for simple 'enum' declaration.

--- swizzle/tests/unit_test/Parser-UT.cpp
+++ swizzle/tests/unit_test/Parser-UT.cpp
@@ -2,6 +2,8 @@
 
 #include <swizzle/ast/Matcher.hpp>
 #include <swizzle/ast/nodes/Comment.hpp>
+#include <swizzle/ast/nodes/Enum.hpp>
+#include <swizzle/ast/nodes/EnumField.hpp>
 #include <swizzle/ast/nodes/Extern.hpp>
 #include <swizzle/ast/nodes/Import.hpp>
 #include <swizzle/ast/nodes/MultilineComment.hpp>
@@ -309,5 +311,62 @@ namespace {
 
         const auto& nameSpace = static_cast<nodes::Namespace&>(*node);
         CHECK_EQUAL("foo::bar::baz", nameSpace.info().token().to_string());
+    }
+
+    struct WhenInputIsEnum : public ParserFixture
+    {
+        const boost::string_view sv = boost::string_view(
+            "namespace foo;" "\n"
+            "enum Metal : u8 {" "\n"
+            "\t" "iron," "\n"
+            "\t" "copper," "\n"
+            "\t" "gold," "\n"
+            "}"
+        );
+    };
+
+    TEST_FIXTURE(WhenInputIsEnum, verifyConsume)
+    {
+        auto matcher = Matcher().getChildrenOf<nodes::Enum>().bind("enum");
+        CHECK(!matcher(parser.ast().root()));
+
+        tokenize(sv);
+        parse();
+
+        CHECK(matcher(parser.ast().root()));
+
+        const auto node = matcher.bound("enum_0");
+        REQUIRE CHECK(node);
+
+        const auto& Enum = static_cast<nodes::Enum&>(*node);
+        CHECK_EQUAL("foo::Metal", Enum.name());
+        CHECK_EQUAL("u8", Enum.underlying().token().to_string());
+
+        auto fieldsMatcher = Matcher().getChildrenOf<nodes::EnumField>().bind("fields");
+        CHECK(fieldsMatcher(node));
+
+        const auto field0 = fieldsMatcher.bound("fields_0");
+        REQUIRE CHECK(field0);
+
+        const auto& f0 = static_cast<nodes::EnumField&>(*field0);
+        CHECK_EQUAL("iron", f0.name().token().to_string());
+        CHECK_EQUAL("u8", f0.underlying().token().to_string());
+        CHECK_EQUAL(0U, boost::get<std::uint8_t>(f0.value()));
+
+        const auto field1 = fieldsMatcher.bound("fields_1");
+        REQUIRE CHECK(field1);
+
+        const auto& f1 = static_cast<nodes::EnumField&>(*field1);
+        CHECK_EQUAL("copper", f1.name().token().to_string());
+        CHECK_EQUAL("u8", f1.underlying().token().to_string());
+        CHECK_EQUAL(1U, boost::get<std::uint8_t>(f1.value()));
+
+        const auto field2 = fieldsMatcher.bound("fields_2");
+        REQUIRE CHECK(field2);
+
+        const auto& f2 = static_cast<nodes::EnumField&>(*field2);
+        CHECK_EQUAL("gold", f2.name().token().to_string());
+        CHECK_EQUAL("u8", f2.underlying().token().to_string());
+        CHECK_EQUAL(2U, boost::get<std::uint8_t>(f2.value()));
     }
 }

64a06f82fba9f50c8537ead400e4b0d235cb5f5a
Austin Gilbert <ceretullis@gmail.com>
2017-08-01 23:39:40 -0500

    ast: nodes: streamline DefaultValue

--- swizzle/ast/nodes/DefaultValue.hpp
+++ swizzle/ast/nodes/DefaultValue.hpp
@@ -15,18 +15,13 @@ namespace swizzle { namespace ast { namespace nodes {
     public:
         DefaultValue(const lexer::TokenInfo& defaultValueInfo, const lexer::TokenInfo& underlyingType);
 
-        const lexer::TokenInfo& info() const;
-        const lexer::TokenInfo& underlying() const;
-
-        void value(const lexer::TokenInfo& valueToken);
         const lexer::TokenInfo& value() const;
+        const lexer::TokenInfo& underlying() const;
 
         void accept(VisitorInterface& visitor) override;
 
     private:
-        const lexer::TokenInfo info_;
+        const lexer::TokenInfo value_;
         const lexer::TokenInfo underlying_;
-
-        lexer::TokenInfo value_;    // literal value
     };
 }}}

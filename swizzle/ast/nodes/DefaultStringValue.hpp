f425bd817f319c6c930c39b6fba017a67a6c522a
Austin Gilbert <ceretullis@gmail.com>
2017-08-06 21:44:05 -0500

    ast: nodes: ast::nodes::DefaultStringValue has to take underlying type as a std::string because that's the type we have on ast::nodes::StructField

--- swizzle/ast/nodes/DefaultStringValue.hpp
+++ swizzle/ast/nodes/DefaultStringValue.hpp
@@ -14,23 +14,18 @@ namespace swizzle { namespace ast { namespace nodes {
     class DefaultStringValue : public Node
     {
     public:
-        DefaultStringValue(const lexer::TokenInfo& defaultValueInfo, const lexer::TokenInfo& underlyingType, const std::ptrdiff_t length);
+        DefaultStringValue(const lexer::TokenInfo& value, const std::string& underlyingType, const std::ptrdiff_t length);
 
-        const lexer::TokenInfo& info() const;
-        const lexer::TokenInfo& underlying() const;
-
-        void value(const lexer::TokenInfo& valueToken);
         const lexer::TokenInfo& value() const;
+        const std::string& underlying() const;
 
         std::ptrdiff_t length() const;
 
         void accept(VisitorInterface& visitor) override;
 
     private:
-        const lexer::TokenInfo info_;
-        const lexer::TokenInfo underlying_;
+        const lexer::TokenInfo value_;
+        const std::string underlying_;
         const std::ptrdiff_t length_;
-
-        lexer::TokenInfo value_;    // string literal value
     };
 }}}

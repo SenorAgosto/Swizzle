35e10f4e36badd65b5a61f43e093cf23d7bf0b66
Austin Gilbert <ceretullis@gmail.com>
2017-08-06 21:47:24 -0500

    parser: states: implementing StructFieldEqualReadState

---
+++ swizzle/parser/states/StructFieldEqualReadState.hpp
@@ -0,0 +1,11 @@
+#pragma once 
+#include <swizzle/parser/ParserStateInterface.hpp>
+
+namespace swizzle { namespace parser { namespace states {
+
+    class StructFieldEqualReadState : public ParserStateInterface
+    {
+    public:
+        ParserState consume(const lexer::TokenInfo& token, NodeStack& nodeStack, NodeStack& attributeStack, TokenStack& tokenStack, ParserStateContext& context) override;
+    };
+}}}

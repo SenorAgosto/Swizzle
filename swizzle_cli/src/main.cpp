6e403114609d2e98ccbc86cb9fabbb0b23b8e5ed
Austin Gilbert <ceretullis@gmail.com>
2018-01-16 22:49:36 -0600

    code cleanup

--- swizzle_cli/src/main.cpp
+++ swizzle_cli/src/main.cpp
@@ -27,7 +27,7 @@ namespace swizzle {
         boost::program_options::variables_map vars;
     };
     
-    Config parseConfig(const int argc, char const * const argv[])
+    Config parse_config(const int argc, char const * const argv[])
     {
         namespace po = boost::program_options;
         Config config;
@@ -44,9 +44,20 @@ namespace swizzle {
         po::store(po::command_line_parser(argc, argv).options(config.description).positional(positionalArguments).run(), config.vars);
         po::notify(config.vars);
         
+        if(argc < 2)
+        {
+            std::cerr << config.description << std::endl;
+            throw std::runtime_error("Exception: too few command line arguments");
+        }
+        
         return config;
     }
 
+    bool validate_file(const boost::filesystem::path& file)
+    {
+        return boost::filesystem::exists(file) && !boost::filesystem::is_directory(file);
+    }
+    
     // TODO: memory map the file and put it into a wield message
     std::string load_file(const std::string& name)
     {
@@ -61,25 +72,115 @@ namespace swizzle {
         
         return std::string(bytes.data(), fileSize);
     }
+    
+    struct CreateTokenCallback
+    {
+        CreateTokenCallback(std::deque<lexer::TokenInfo>& tokens)
+            : tokens_(tokens)
+        {
+        }
+
+        void operator()(const lexer::TokenInfo& token)
+        {
+            tokens_.push_back(token);
+        }
+
+    private:
+        std::deque<lexer::TokenInfo>& tokens_;
+    };
+    
+    template<typename Callback>
+    void tokenize(lexer::Tokenizer<Callback>& tokenizer, const boost::string_view& sv)
+    {
+        for(std::size_t position = 0, end = sv.length(); position < end; ++position)
+        {
+            tokenizer.consume(sv, position);
+        }
+
+        tokenizer.flush();
+    }
+
+    void parse(parser::Parser& parser, const std::deque<lexer::TokenInfo>& tokens)
+    {
+        for(const auto token : tokens)
+        {
+            parser.consume(token);
+        }
+
+        parser.finalize();
+    }
+    
+    int validate_config(const Config& config)
+    {
+        if(config.vars.count("help"))
+        {
+            std::cout << config.description << std::endl;
+            return 0;
+        }
+        
+        if(config.file.string().empty())
+        {
+            std::cout << config.description << std::endl;
+            return -1;
+        }
+        
+        if(config.vars.count("list-backends"))
+        {
+            // TODO: implement
+            return 0;
+        }
+       
+        if(!validate_file(config.file))
+        {
+            std::cerr << config.file << " does not exist or is a directory" << std::endl;
+            return -1;
+        }
+        
+        return 0;
+    }
+    
+    void process(const Config& config)
+    {
+        std::deque<lexer::TokenInfo> tokens;
+        CreateTokenCallback callback = CreateTokenCallback(tokens);
+        
+        lexer::Tokenizer<CreateTokenCallback> tokenizer = lexer::Tokenizer<CreateTokenCallback>(config.file.string(), callback);
+        parser::Parser parser;
+
+        std::string file = load_file(config.file.string());
+        boost::string_view sv = boost::string_view(file);
+        
+        tokenize(tokenizer, sv);
+        parse(parser, tokens);
+        
+        // TODO: pass AST to backend
+        // parser.ast().root()
+    }
 }
 
 using namespace swizzle;
 
 int main(const int argc, char const * const argv[])
 {
-    Config config = parseConfig(argc, argv);
-    if((argc < 2) || (config.vars.count("help")) || (config.file.string().empty()))
+    try
     {
-        std::cout << config.description << std::endl;
-        return 0;
+        const Config config = parse_config(argc, argv);
+        
+        const int status = validate_config(config);
+        if(status != 0) return status;
+        
+        process(config);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
+    catch(...)
+    {
+        std::cerr << "FATAL ERROR, exiting" << std::endl;
+        return -1;
     }
     
-    if(config.vars.count("list-backends"))
-    {
-        // TODO: implement
-        return 0;
-    }
-    
-    std::cout << "backend: " << config.backend << ", file: " << config.file.string() << std::endl;
     return 0;
 }

5622958aa2dc93805a06be97b12ed9f752b90594
Austin Gilbert <ceretullis@gmail.com>
2018-02-04 12:35:27 -0600

    types: utils: just test that we can read a float from istream... NOTE: we are not checking overflow at this time.

--- swizzle/src/types/utils/CheckFloatValue.cpp
+++ swizzle/src/types/utils/CheckFloatValue.cpp
@@ -1,10 +1,27 @@
 #include <swizzle/lexer/TokenInfo.hpp>
+#include <swizzle/Exceptions.hpp>
+
+#include <sstream>
 
 namespace swizzle { namespace types { namespace utils {
 
     void checkFloatValue(const boost::string_view& underlying, const lexer::TokenInfo& token, const std::string& errorMessage)
     {
-        #error TODO implement
+        if(underlying == "f32")
+        {
+            float f = 0.0;
+            std::stringstream ss(token.token().value().to_string());
+            ss >> f;
+        }
+        
+        if(underlying == "f64")
+        {
+            double d = 0.0;
+            std::stringstream ss(token.token().value().to_string());
+            ss >> d;
+        }
+        
+        throw SyntaxError(errorMessage + ": " + underlying.to_string(), token);
     }
 }}}
 

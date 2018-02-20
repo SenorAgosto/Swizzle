4cda3590c1bc7533610e8a3638d78d322f9a27ec
Austin Gilbert <ceretullis@gmail.com>
2018-02-19 22:01:34 -0600

    parser: states: - bug fix: had 'nodeStack' parameter name where attribute stack is. - changes to creation of ast::node::BaseClass following changes to its definition

--- swizzle/src/parser/states/StructBaseNameOrNamespaceReadState.cpp
+++ swizzle/src/parser/states/StructBaseNameOrNamespaceReadState.cpp
@@ -1,14 +1,18 @@
 #include <swizzle/parser/states/StructBaseNameOrNamespaceReadState.hpp>
 
 #include <swizzle/ast/nodes/BaseClass.hpp>
+#include <swizzle/ast/nodes/Struct.hpp>
+
 #include <swizzle/Exceptions.hpp>
 #include <swizzle/parser/ParserStateContext.hpp>
 #include <swizzle/types/utils/AppendNode.hpp>
+#include <swizzle/types/utils/AttachAttributes.hpp>
 #include <swizzle/types/utils/CreateType.hpp>
+#include <swizzle/types/utils/NodeStackTopIs.hpp>
 
 namespace swizzle { namespace parser { namespace states {
 
-    ParserState StructBaseNameOrNamespaceReadState::consume(const lexer::TokenInfo& token, types::NodeStack&, types::NodeStack& nodeStack, types::TokenStack& tokenStack, ParserStateContext& context)
+    ParserState StructBaseNameOrNamespaceReadState::consume(const lexer::TokenInfo& token, types::NodeStack& nodeStack, types::NodeStack& attributeStack, types::TokenStack& tokenStack, ParserStateContext& context)
     {
         const auto type = token.token().type();
         
@@ -20,27 +24,33 @@ namespace swizzle { namespace parser { namespace states {
         if(type == lexer::TokenType::l_brace)
         {
             const auto base_class = types::utils::createType(tokenStack);
-        
-            if(context.SymbolTable.contains(context.CurrentNamespace, base_class.token().to_string()))
+            const auto info = context.SymbolTable.find(context.CurrentNamespace, base_class.token().to_string(), SyntaxError("Base class type not defined", token));
+            
+            if(types::utils::nodeStackTopIs<ast::nodes::Struct>(nodeStack))
             {
-                types::utils::appendNode<ast::nodes::BaseClass>(nodeStack, base_class);
+                auto node = types::utils::appendNode<ast::nodes::BaseClass>(nodeStack, static_cast<ast::nodes::Struct&>(*info.node()));
+                types::utils::attachAttributes(attributeStack, node);
+                
                 return ParserState::StructStartScope;
             }
-        
-            throw SyntaxError("Base class type not defined", token);
+    
+            throw ParserError("Internal parser error, expected top of node stack to be ast::nodes::Struct");
         }
 
         if(type == lexer::TokenType::comma)
         {
             const auto base_class = types::utils::createType(tokenStack);
+            const auto info = context.SymbolTable.find(context.CurrentNamespace, base_class.token().to_string(), SyntaxError("Base class type not defined", token));
             
-            if(context.SymbolTable.contains(context.CurrentNamespace, base_class.token().to_string()))
+            if(types::utils::nodeStackTopIs<ast::nodes::Struct>(nodeStack))
             {
-                types::utils::appendNode<ast::nodes::BaseClass>(nodeStack, base_class);
+                auto node = types::utils::appendNode<ast::nodes::BaseClass>(nodeStack, static_cast<ast::nodes::Struct&>(*info.node()));
+                types::utils::attachAttributes(attributeStack, node);
+                
                 return ParserState::StructBaseColonRead;
             }
             
-            throw SyntaxError("Inheriting from undefined type", token);
+            throw ParserError("Internal parser error, expected top of node stack to be ast::nodes::Struct");
         }
 
         throw SyntaxError("Expected ':'", token);

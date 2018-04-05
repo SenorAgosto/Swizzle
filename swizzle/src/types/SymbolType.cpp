4f3a9ec773d77edfcd0d6541a881a2a73cc6bd71
Austin Gilbert <agilbert@exegy.com>
2018-04-05 15:18:04 -0500

    types: adding to_string(const SymbolType)

---
+++ swizzle/src/types/SymbolType.cpp
@@ -0,0 +1,25 @@
+#include <swizzle/types/SymbolType.hpp>
+
+namespace swizzle { namespace types {
+
+    std::string to_string(const SymbolType type)
+    {
+        switch(type)
+        {
+        case SymbolType::Bitfield: return "SymbolType::Bitfield";
+        case SymbolType::BitfieldField: return "SymbolType::BitfieldField";
+        case SymbolType::Enum: return "SymbolType::Enum";
+        case SymbolType::EnumField: return "SymbolType::EnumField";
+        case SymbolType::Extern: return "SymbolType::Extern";
+        case SymbolType::Import: return "SymbolType::Import";
+        case SymbolType::Struct: return "SymbolType::Struct";
+        case SymbolType::StructField: return "SymbolType::StructField";
+        case SymbolType::Namespace: return "SymbolType::Namespace";
+        case SymbolType::TypeAlias: return "SymbolType::TypeAlias";
+        case SymbolType::VariableBlock: return "SymbolType::VariableBlock"; 
+        }
+
+        throw std::runtime_error("Unknown SymbolType");
+    }
+}}
+
